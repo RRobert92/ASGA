6fda0489e19962f3611a9d0e6ac0247b26fbeddf
RRobert92 <robert.kiewisz@gmil.com>
2019-11-01 17:36:47 +0100

    Replaced GUI with Shiny Dashboard + cleaned code

--- MT_length_distribtion/app.R
+++ MT_length_distribtion/app.R
@@ -1,153 +1,143 @@
-#library for this app
+library(shinydashboard)
 library(shiny)
-library(shinythemes)
 library(readxl)
 library(tidyverse)
-##library(shinydashboard)
 
-## Set file size lmit to 500mb 
+##Maximum size of memory used by R, set to 500mb
 options(shiny.maxRequestSize = 500*1024^2)
-## Define UI for application
-ui <- fluidPage(
-  
-  # App title ----
-  navbarPage("MTs Length distribution", theme = shinytheme("slate"),
-             tabPanel("Info", 
-                        navlistPanel(
-                          tabPanel("About", includeHTML("about.html")),
-                          tabPanel("User Manual", includeHTML("manual.html")),
-                          tabPanel("Resource", includeHTML("resource.html"))
-                        )),
-                    
-             tabPanel("Upload", 
-                      sidebarLayout(
-                        
-                        # Sidebar panel for inputs ----
-                        sidebarPanel(
-                          
-                          # Input: Select a sigle file ----
-                          fileInput("Amirafile", "Choose .xlsx File", multiple = FALSE,
-                                    accept = c("text/xlsx", "text/comma-separated-values,text/plain", ".xlsx")),
-                          
-                          # Horizontal space line ----
-                          tags$hr(),
-                          
-                          # Input: Select number of rows to display ----
-                          radioButtons("disp", "Display", choices = c(Head = "head", All = "all"), selected = "head"),
-                        ),
-                        
-                        # Main panel for displaying outputs ----
-                        mainPanel(tabsetPanel(
-                          tabPanel("Nodes", tableOutput("data.node")),
-                          tabPanel("Points", tableOutput("data.point")),
-                          tabPanel("Segments", tableOutput("data.segment"))
-                        )),
-                      )),
-             
-             tabPanel("Data",
-                      sidebarPanel(
-                       
-                                 selectInput("analysis", h3("Select Analysis type"), 
-                                             choices = list("Histogram Distribtuion" = 1,
-                                                            "LOgaritmic Disribtuin" = 2), selected = 1),
-                                 conditionalPanel(
-                                   condition = "input.analysis == 1",
-                                               numericInput("bin.min", "Bin start from (um):", value = 0),
-                                               numericInput("bin.max", "Bin stop at (um):", value = 10),
-                                               numericInput("bin.step", "Bin every (um);", value = 0.5)
-                                 )
-                                 ),
-                                 
-                      
-                      mainPanel(
-                        
-                        plotOutput(outputId = "length.plot_kmts"),
-                        plotOutput(outputId = "length.plot_nonkmts")
-                      )
-                    ),
-                      
-             tabPanel("Export", "Work in progress")
-  ))
 
-## Define server logic to read selected file ----
+ui <- dashboardPage(
+  dashboardHeader(title = "Length Distribtion"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About", tabName = "menu_about", icon = icon("info-circle")),
+      menuItem("Instruction", tabName = "menu_instruction", icon = icon("chalkboard-teacher")),
+      menuItem("Upload", tabName = "menu_upload", icon = icon("upload")),
+      menuItem("Analysis", tabName = "menu_analysis", icon = icon("diagnoses")),
+      menuItem("Export", tabName = "menu_export", icon = icon("download")),
+      menuItem("Resource", tabName = "menu_resource", icon = icon("github"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      
+      tabItem(tabName = "menu_about",
+              includeHTML("about.html")
+      ),
+      
+      tabItem(tabName = "menu_instruction",
+              includeHTML("manual.html")
+      ),
+      
+      tabItem(tabName = "menu_upload",
+              fluidRow(
+                box(title = "Upload file", width = 3,
+                    fileInput("amirafile", "Choose .xlsx File", multiple = FALSE,
+                              accept = c("text/xlsx", "text/comma-separated-values,text/plain", ".xlsx")),
+                ),
+                box(title = "Lodate data:", width = 9,
+                    radioButtons("disp", "Display", choices = c(Head = "head", All = "all"), selected = "head"),
+                    tabsetPanel(
+                      tabPanel("Nodes", tableOutput("data.node")),
+                      tabPanel("Points", tableOutput("data.point")),
+                      tabPanel("Segments", tableOutput("data.segment"))
+                    )))
+      ),
+      
+      tabItem(tabName = "menu_analysis",
+              fluidRow(
+                box(title = "Analysis Parameaters", width = 2,
+                    selectInput("analysis", h3("Select Analysis type"), 
+                                choices = list("Histogram Distribtuion" = 1,
+                                               "Logaritmic Disribtuin" = 2), selected = 1),
+                    conditionalPanel(
+                      condition = "input.analysis == 1",
+                      numericInput("bin.min", "Bin start from (um):", value = 0),
+                      numericInput("bin.max", "Bin stop at (um):", value = 10),
+                      numericInput("bin.step", "Bin every (um);", value = 0.5)),
+                ),
+                
+                box(title = "Graphs", width = 10,
+                    plotOutput(outputId = "length.plot_kmts"))
+              )
+      ),
+      tabItem(tabName = "menu_export",
+              
+      ),
+      tabItem(tabName = "menu_resource",
+              includeHTML("manual.html")
+      )
+    )
+  )
+)
+
 server <- function(input, output) {
   
   output$data.node <- renderTable({
-    req(input$Amirafile)
+    req(input$amirafile)
     
-    # this will load specific datasheet from the Amira file - file have to be converted from XML to xlsx file
     tryCatch({
-      Nodes <- read_excel(input$Amirafile$datapath, sheet = "Nodes")
+      Nodes <- read_excel(input$amirafile$datapath, sheet = "Nodes")
     })
-    
-    #Display table with only few first line for Nodes sheet
     if (input$disp == "head") {
       return(head(Nodes %>% select("Node ID", "X Coord", "Y Coord", "Z Coord")))
     }
-    
     else{
       return(Nodes)
     }
   })
   
   output$data.point <- renderTable({
-    req(input$Amirafile)
+    req(input$amirafile)
     
     tryCatch({
-      Points <- read_excel(input$Amirafile$datapath, sheet = "Points")
+      Points <- read_excel(input$amirafile$datapath, sheet = "Points")
     })
-    
-    #Display table with only few first line for Points sheet
     if (input$disp == "head") {
       return(head(Points %>% select("Point ID", "X Coord", "Y Coord", "Z Coord")))
     }
-    
     else{
       return(Points)
     }
   })
   
   output$data.segment <- renderTable({
-    req(input$Amirafile)
+    req(input$amirafile)
     
     tryCatch({
-      Segments <- read_excel(input$Amirafile$datapath, sheet = "Segments")
+      Segments <- read_excel(input$amirafile$datapath, sheet = "Segments")
     })
-    
-    #Display table with only few first line for Nodes sheet
     if (input$disp == "head") {
       return(head(Segments %>% select("Segment ID", "length", "Node ID #1", "Node ID #2")))
     }
-    
     else{
       return(Segments)
     }
   })
   
-  
-
-output$length.plot_kmts <- renderPlot({
-  req(input$Amirafile)
-  req(input$analysis)
-  req(input$bin.min)
-  req(input$bin.max)
-  req(input$bin.step)
-  tryCatch({
-    Segments <- read_excel(input$Amirafile$datapath, sheet = "Segments")
-  })
-  kmts <- Segments %>% filter_at(vars(starts_with("POle")), any_vars(.>= 1))
-  non_kmts <- setdiff(Segments, kmts)
-  if(input$analysis == 1){
-  xkmts <- kmts$length/10000
-  xnon_kmts <- non_kmts$length/10000
-  bins <- seq(min(input$bin.min), max(input$bin.max), length.out = (input$bin.max/input$bin.step) + 1)
-  hist(xnon_kmts, breaks = bins, col = "yellow", xlab = "Length (um)", ylab = "No. of MTs")
-  hist(xkmts, breaks = bins, col = "red",add=T)
-  }
-  if(input$analysis == 2){
+  output$length.plot_kmts <- renderPlot({
+    req(input$amirafile)
+    req(input$analysis)
+    req(input$bin.min)
+    req(input$bin.max)
+    req(input$bin.step)
     
-  }
-})
+    tryCatch({
+      Segments <- read_excel(input$amirafile$datapath, sheet = "Segments")
+    })
+    kmts <- Segments %>% filter_at(vars(starts_with("POle")), any_vars(.>= 1))
+    non_kmts <- setdiff(Segments, kmts)
+    if(input$analysis == 1){
+      xkmts <- kmts$length/10000
+      xnon_kmts <- non_kmts$length/10000
+      bins <- seq(min(input$bin.min), max(input$bin.max), length.out = (input$bin.max/input$bin.step) + 1)
+      hist(xnon_kmts, breaks = bins, col = "yellow", xlab = "Length (um)", ylab = "No. of MTs")
+      hist(xkmts, breaks = bins, col = "red",add=T)
+    }
+    if(input$analysis == 2){
+      
+    }
+  })
 }
-# Run the application
+
 shinyApp(ui = ui, server = server)
