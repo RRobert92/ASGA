b5d53f4cdf053df5e037ccf0ac11467661173191
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2019-11-15 16:31:13 +0100

    Create Algorithm.R

---
+++ bin/Minus_ends_ position_Along_Spindle_Axis/Algorithm.R
@@ -0,0 +1,11 @@
+## Position of minus ends
+## Select KMTs
+
+## Select Segments and Nodes sheets
+
+## Select Pole1 nodes
+
+## calculate distance of node#1 and node#2 and select closest to the pole <- data.frame(minus_ends, xyz coord (um))
+
+## Plot minus ends distribution on X, Y or Z axis
+
