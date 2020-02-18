74a9b2da33686bd13d0af54377101f9c4ee77677
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2020-02-18 11:44:31 +0100

    Delete Find_XYZ.R

--- bin/The cluster of KMTs analysis tools/Functions/Find_XYZ.R
+++
@@ -1,8 +0,0 @@
-## combin point_id with xyz
-## x is a number of a column contain KMTs information like Pole1_00_1
-Find_XYZ <- function(x) {
-  joined_data <- join_all(list(x, Points),
-                          by = "Point_ID")
-  mutate_all(joined_data, 
-             function(y) as.numeric(as.character(y)))
-}
