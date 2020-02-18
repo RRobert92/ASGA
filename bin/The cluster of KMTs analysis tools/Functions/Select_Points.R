6f5965314875bb9633f9ae37898eaa8ac0b35668
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2020-02-18 11:44:49 +0100

    Delete Select_Points.R

--- bin/The cluster of KMTs analysis tools/Functions/Select_Points.R
+++
@@ -1,7 +0,0 @@
-## remove "," and spread numbers in single cells
-## x number of the KMTs in the fiber
-## y is number of a column contain information about KTMs like Pole1_00
-Select_Points <- function(x, y) {
-  selected_points <- data.frame(str_split(gsub("[^[:digit:]]", ",", y[x, 2]), pattern = ","))
-  points <- data.frame(Point_ID = selected_points[, 1])
-}
