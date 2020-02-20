d5d375ce0c7433d3cb460bef320f3df2f8045a30
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2020-02-20 10:24:33 +0100

    ...

--- bin/The cluster of KMTs analysis tools/Utility/Check_Data.R
+++ bin/The cluster of KMTs analysis tools/Utility/Check_Data.R
@@ -3,8 +3,10 @@ Test_Poles <- colnames(Nodes %>% select(Pole1)) == "Pole1" && colnames(Nodes %>%
 
 if (Test_Poles && Test_Segments == TRUE) {
   msg_box("The data structure looks great! Press Ok to analyze your awesome data!")
+  
 } else if (Test_Segments == FALSE && Test_Poles == TRUE) {
   quit(msg_box("The Segments data structure looks strange! Please check it with the guidelines and try again."))
+  
 } else if (Test_Poles == FALSE && Test_Segments == TRUE) {
   msg_box("Could not find 'Poles' coordinates in the Nodes excel sheet! Please check it with the guidelines and try again.")
   dlg_message("Are the 'Pole's' coordinates embedded in the raw data in the Node sheet?",
@@ -15,8 +17,10 @@ if (Test_Poles && Test_Segments == TRUE) {
                        "Pole1")$res
     Pole2 <- dlg_input("Is this correct label name for the 'Pole_2'?", 
                        "Pole2")$res
+    
   } else if (res == "No") {
-    quit(msg_box("Please add 'Pole_1' and 'Pole_2' coordinate in the Nodes sheet and try again."))
+    quit(msg_box("Please add 'Pole_1' and 'Pole_2' coordinates in the Nodes sheet and try again."))
+    
   }
 }
 rm(Test_Segments, 
