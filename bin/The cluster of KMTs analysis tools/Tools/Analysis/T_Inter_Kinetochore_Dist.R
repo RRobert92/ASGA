4435f0f77abbe5715e1ccf629a8808b7e3c9f4e9
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2020-02-20 18:28:40 +0100

    Changed algorithm to count distance

--- bin/The cluster of KMTs analysis tools/Tools/Analysis/T_Inter_Kinetochore_Dist.R
+++ bin/The cluster of KMTs analysis tools/Tools/Analysis/T_Inter_Kinetochore_Dist.R
@@ -1,49 +1,49 @@
-#########################################################
-# The analysis tool to count inter kinetochore distance #
-#########################################################
-## x variable in the function is a number of a KMTs in the fiber (e.g. 1, 2, 3... etc.)
-
-## Get a possition of the all kinetochore
-Kinetochore_XYZ <- function(x) {
-  Plus_end <- data.frame()
-  for (i in 1:nrow(get(colnames(Segments)[x]))){
-    Plus_end[i,1:3] <- get(paste(colnames(Segments)[x], 
-                                 i, 
-                                 sep = "_"))[1,2:4]
-  }
-  Plus_end <- data.frame(X_Median = c(median(as.matrix(Plus_end[1]))),
-                         Y_Median = c(median(as.matrix(Plus_end[2]))),
-                         Z_Median = c(median(as.matrix(Plus_end[3]))))
-}
-
 ## Used output from Kinetochore_XYZ function (Kinetochore_Pole1, Kineetochore_Pole2)
 ## x variable is fixed Kinetochore_Pole1
 Inter_Kinetochore_Dist <- function(){
   
-  total <- nrow(Kinetochore_Pole1)
-  pb <- winProgressBar(min = 0,
+  total <- length(which(colnames(Segments) == "Pole1_00") : which(colnames(Segments) == colnames(Segments %>% select(starts_with("Pole1")))[length(colnames(Segments %>% select(starts_with("Pole1"))))]))
+  pb <- winProgressBar(min = 2,
                        max =  total,
                        width = 420)
   
   Inter_Kinetochore_Distance <- data.frame()
   
   ## Get distance to each kinetochore on the oposit pole
-  for(i in 1:nrow(Kinetochore_Pole1)){
+  for(i in which(colnames(Segments) == "Pole1_00") : which(colnames(Segments) == colnames(Segments %>% select(starts_with("Pole1")))[length(colnames(Segments %>% select(starts_with("Pole1"))))])){
     tryCatch({
-       DF <- data.frame()
+       DF_Pole1 <- colnames(Segments)[i]
+       DF <- data.frame(str_split(gsub("[^[:digit:]]", "Pole_1", DF_Pole1), pattern = "Pole_1"))
+       DF_Pole2 <- paste("Pole2", DF[6,1], sep = "_")
+       
+       Plus_end_1 <- data.frame()
+     for (j in 1:nrow(get(DF_Pole1))) {
+       
+       Plus_end_1[j,1:3] <- get(paste(DF_Pole1, 
+                                    j, 
+                                    sep = "_"))[1,2:4]
+    }
+    Plus_end_1 <- data.frame(X_Median = c(median(as.matrix(Plus_end_1[1]))),
+                             Y_Median = c(median(as.matrix(Plus_end_1[2]))),
+                             Z_Median = c(median(as.matrix(Plus_end_1[3]))))
+       
+    Plus_end_2 <- data.frame()
+    for (j in 1:nrow(get(DF_Pole2))) {
+      
+      Plus_end_2[j,1:3] <- get(paste(DF_Pole2, 
+                                     j, 
+                                     sep = "_"))[1,2:4]
+    }
+    Plus_end_2 <- data.frame(X_Median = c(median(as.matrix(Plus_end_2[1]))),
+                             Y_Median = c(median(as.matrix(Plus_end_2[2]))),
+                             Z_Median = c(median(as.matrix(Plus_end_2[3]))))
     
-     ## Get oposit pole
-      DF[1:nrow(Kinetochore_Pole2),1:3] <- Kinetochore_Pole1[i,1:3]
-      positions <- cbind(DF, Kinetochore_Pole2)
+      
     
     ## Calculate distance to each oposit kinetochore and save smales distance
-    positions$distance <- apply(positions, 1, function(z) dist(matrix(z, nrow = 2, byrow = TRUE)))
-    Inter_Kinetochore_Distance[i,1] <- min(positions$distance)
-    
-    ## Remove kinetochore wich was found on the oposit pole
-    Kinetochore_Pole2 [which.min(positions$distance), 1:3] <- NA
-    Kinetochore_Pole2 <- na.omit(Kinetochore_Pole2)
-    
+    positions <- sqrt((Plus_end_1[1,1] - Plus_end_2[1,1])^2 + (Plus_end_1[1,2] - Plus_end_2[1,2])^2 + (Plus_end_1[1,3] - Plus_end_2[1,3])^2)
+    Inter_Kinetochore_Distance[i,1] <- round(positions, 5)
+ 
     Sys.sleep(0.1)
     setWinProgressBar(pb, i, 
                       title = paste("Finding kinetochore pair and getting distance...",
@@ -56,5 +56,6 @@ Inter_Kinetochore_Dist <- function(){
   }
   
   close(pb)
-  Inter_Kinetochore_Distance
+  DF <- Inter_Kinetochore_Distance
+  na.omit(DF)
 }
