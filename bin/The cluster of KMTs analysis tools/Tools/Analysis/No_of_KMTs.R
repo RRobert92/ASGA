9232fe31348f561daccbd3efd2f2696569744e0e
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2020-02-20 10:58:58 +0100

    Update No_of_KMTs.R

--- bin/The cluster of KMTs analysis tools/Tools/Analysis/No_of_KMTs.R
+++ bin/The cluster of KMTs analysis tools/Tools/Analysis/No_of_KMTs.R
@@ -1,6 +1,6 @@
-###################################
-# No. of KMTs at eahc kinetochore #
-###################################
+##############################################################
+# The analysis tool to count no. of KMTs at each kinetochore #
+##############################################################
 ## x variable is a number of a column contain KMTs information (e.g. 1, 2, 3...., etc.)
 ## Count how many KMTs exist for each label
 
