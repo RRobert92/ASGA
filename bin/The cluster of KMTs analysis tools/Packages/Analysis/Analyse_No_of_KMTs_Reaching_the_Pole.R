a96d6bd006e6c37915b24e681f52c0ca7784e4b1
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2020-03-10 12:21:53 +0100

    Separate analysis for pole 1 and 2

--- bin/The cluster of KMTs analysis tools/Packages/Analysis/Analyse_No_of_KMTs_Reaching_the_Pole.R
+++ bin/The cluster of KMTs analysis tools/Packages/Analysis/Analyse_No_of_KMTs_Reaching_the_Pole.R
@@ -1,20 +1,28 @@
-##################################################################
-# The analysis tool count no. of KMTs with a (-) end at the pole #
-##################################################################
+###################################################################
+# The analysis tool count no. of KMTs with a (-) end at the pole1 #
+###################################################################
 ## x variablein in the function is a number of a column contain KMTs information (e.g. 1, 2, 3...., etc.)
 ## Count how many KMTs with a minus end distance of "Minus_Threshold" is in the fiber
 
-KMTs_at_the_Pole <- KMTs_to_the_Pole(which(colnames(Segments) == "Pole1_00"))
-names(KMTs_at_the_Pole)[1] <- "No. of KMTs"
+######################
+# Setting for Pole_2 #
+######################
 
-KMTs_to_the_Pole_and_length <- KMTs_to_the_Pole_vs_length(which(colnames(Segments) == "Pole1_00"))
-names(KMTs_to_the_Pole_and_length)[1] <- "No. of KMTs"
-names(KMTs_to_the_Pole_and_length)[2] <- "KMTs length"
-names(KMTs_to_the_Pole_and_length)[3] <- "Minus end dist."
-names(KMTs_to_the_Pole_and_length)[4] <- "Plus end dist. to k-core"
-names(KMTs_to_the_Pole_and_length)[5] <- "Plus end dist. to pole"
+KMTs_at_the_Pole1 <- KMTs_to_the_Pole(which(colnames(Segments) == "Pole1_00"))
+names(KMTs_at_the_Pole1)[1] <- "No. of KMTs"
 
-total <- as.numeric(ncol(Segments %>% select(starts_with("Pole"))))
+KMTs_to_the_Pole1_and_length <- KMTs_to_the_Pole_vs_length(which(colnames(Segments) == "Pole1_00"))
+names(KMTs_to_the_Pole1_and_length)[1] <- "No. of KMTs"
+names(KMTs_to_the_Pole1_and_length)[2] <- "KMTs length"
+names(KMTs_to_the_Pole1_and_length)[3] <- "Minus end dist."
+names(KMTs_to_the_Pole1_and_length)[4] <- "Plus end dist. to k-core"
+names(KMTs_to_the_Pole1_and_length)[5] <- "Plus end dist. to pole"
+
+###########################
+# Progress bar for Pole_1 #
+###########################
+
+total <- as.numeric(length(which(colnames(Segments) == "Pole1_00") : as.numeric(which(colnames(Segments) == "Pole2_00"))) - 1)
 pb <- winProgressBar(min = 2,
                      max =  total,
                      width = 400)
@@ -22,12 +30,16 @@ pb <- winProgressBar(min = 2,
 DF1 <- data.frame()
 DF2 <- data.frame()
 
-for (i in which(colnames(Segments) == "Pole1_01") : which(colnames(Segments) == colnames(Segments %>% select(starts_with("Pole")))[ncol(Segments %>% select(starts_with("Pole")))])) {
+##################################################
+# Loop iterating through each KMT for the Pole_1 #
+##################################################
+
+for (i in which(colnames(Segments) == "Pole1_00") : as.numeric(which(colnames(Segments) == "Pole2_00") - 1)) {
   tryCatch({
     assign("DF1",
            KMTs_to_the_Pole(i))
     names(DF1)[1] <- "No. of KMTs"
-    KMTs_at_the_Pole <- rbind(KMTs_at_the_Pole, 
+    KMTs_at_the_Pole1 <- rbind(KMTs_at_the_Pole1, 
                               DF1)
     
     assign("DF2",
@@ -39,13 +51,14 @@ for (i in which(colnames(Segments) == "Pole1_01") : which(colnames(Segments) == 
     names(DF2)[4] <- "Plus end dist. to k-core"
     names(DF2)[5] <- "Plus end dist. to pole"
     
-    KMTs_to_the_Pole_and_length <- rbind(KMTs_to_the_Pole_and_length, 
+    KMTs_to_the_Pole1_and_length <- rbind(KMTs_to_the_Pole1_and_length, 
                                          DF2)
+    KMTs_to_the_Pole1_and_length <- na.omit(KMTs_to_the_Pole1_and_length)
   },
   error = function(e){})
   Sys.sleep(0.1)
   setWinProgressBar(pb, i, 
-                    title = paste("Calculating no. of KMTs reaching the pole...", 
+                    title = paste("Calculating no. of KMTs reaching the pole1...", 
                                   round((i - 1) / total * 100,
                                         0),
                                   "% Done"))
@@ -53,7 +66,75 @@ for (i in which(colnames(Segments) == "Pole1_01") : which(colnames(Segments) == 
 
 close(pb)
 
-KMTs_at_the_Pole <- data.frame(KMTs_at_the_Pole[,1])
-names(KMTs_at_the_Pole)[1] <- "No. of KMTs"
+KMTs_at_the_Pole1 <- data.frame(KMTs_at_the_Pole1[,1])
+names(KMTs_at_the_Pole1)[1] <- "No. of KMTs"
+
+rm(DF2)
+
+######################
+# Setting for Pole_2 #
+######################
+
+KMTs_at_the_Pole2 <- KMTs_to_the_Pole(which(colnames(Segments) == "Pole1_00"))
+names(KMTs_at_the_Pole2)[1] <- "No. of KMTs"
+
+KMTs_to_the_Pole2_and_length <- KMTs_to_the_Pole_vs_length(which(colnames(Segments) == "Pole1_00"))
+names(KMTs_to_the_Pole2_and_length)[1] <- "No. of KMTs"
+names(KMTs_to_the_Pole2_and_length)[2] <- "KMTs length"
+names(KMTs_to_the_Pole2_and_length)[3] <- "Minus end dist."
+names(KMTs_to_the_Pole2_and_length)[4] <- "Plus end dist. to k-core"
+names(KMTs_to_the_Pole2_and_length)[5] <- "Plus end dist. to pole"
+
+###########################
+# Progress bar for Pole_2 #
+###########################
+
+total <- which(colnames(Segments) == colnames(Segments %>% select(starts_with("Pole")))[ncol(Segments %>% select(starts_with("Pole")))]) - as.numeric(which(colnames(Segments) == "Pole2_00") - 1)
+pb <- winProgressBar(min = 0,
+                     max =  total,
+                     width = 400)
+
+DF1 <- data.frame()
+DF2 <- data.frame()
+
+##################################################
+# Loop iterating through each KMT for the Pole_2 #
+##################################################
+
+for (i in which(colnames(Segments) == "Pole2_00") : as.numeric(ncol(Segments) - 4)) {
+  tryCatch({
+    assign("DF1",
+           KMTs_to_the_Pole(i))
+    names(DF1)[1] <- "No. of KMTs"
+    KMTs_at_the_Pole2 <- rbind(KMTs_at_the_Pole2, 
+                               DF1)
+    
+    assign("DF2",
+           KMTs_to_the_Pole_vs_length(i))
+    
+    names(DF2)[1] <- "No. of KMTs"
+    names(DF2)[2] <- "KMTs length"
+    names(DF2)[3] <- "Minus end dist."
+    names(DF2)[4] <- "Plus end dist. to k-core"
+    names(DF2)[5] <- "Plus end dist. to pole"
+    
+    KMTs_to_the_Pole2_and_length <- rbind(KMTs_to_the_Pole2_and_length, 
+                                          DF2)
+    KMTs_to_the_Pole2_and_length <- na.omit(KMTs_to_the_Pole2_and_length)
+  },
+  error = function(e){})
+  
+  Sys.sleep(0.1)
+  setWinProgressBar(pb, i - as.numeric(which(colnames(Segments) == "Pole2_00")), 
+                    title = paste("Calculating no. of KMTs reaching the pole2...",
+                                  round((i - as.numeric(which(colnames(Segments) == "Pole2_00") - 1)) / total * 100,
+                                        0), 
+                                  "% Done"))
+}
+
+close(pb)
+
+KMTs_at_the_Pole2 <- data.frame(KMTs_at_the_Pole2[,1])
+names(KMTs_at_the_Pole2)[1] <- "No. of KMTs"
 
 rm(DF2)
