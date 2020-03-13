509093e3932e2b41f899f933396a000d6e85cffd
RRobert92 <56911280+RRobert92@users.noreply.github.com>
2020-03-13 11:04:09 +0100

    Add execution for the local curvature function

--- bin/The cluster of KMTs analysis tools/Packages/Analysis/P_KMT_Curvature.R
+++ bin/The cluster of KMTs analysis tools/Packages/Analysis/P_KMT_Curvature.R
@@ -37,3 +37,36 @@ for(i in which(colnames(Segments) == "Pole1_01") : as.numeric(ncol(Segments) - 4
 }
 close(pb)
 rm(DF)
+
+####################################
+# Progress bar for local curvature #
+####################################
+
+total <- as.numeric(ncol(Segments %>% select(starts_with("Pole"))))
+pb <- winProgressBar(min = 2,
+                     max =  total,
+                     width = 400)
+
+############################################################
+# Loop iterating through each k-fiiber for local curvature #
+############################################################
+
+KMTs_local_Curvature <- local_curvature(2)
+
+for(i in which(colnames(Segments) == "Pole1_01") : as.numeric(ncol(Segments) - 4)){
+  tryCatch({
+    assign("DF",
+           local_curvature(i))
+    KMTs_local_Curvature <- rbind(KMTs_local_Curvature,
+                                  DF)
+  },
+  error = function(e){})
+  Sys.sleep(0.1)
+  setWinProgressBar(pb, i, 
+                    title = paste("Calculating local curvature of KMTs...",
+                                  round((i - 1) / total * 100,
+                                        0),
+                                  "% Done"))
+}
+close(pb)
+rm(DF)
