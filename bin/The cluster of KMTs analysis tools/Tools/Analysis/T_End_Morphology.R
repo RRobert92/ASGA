4b6d8601a14cb3feac4c675657b8beaff672432f
RRobert92 <robert.kiewisz@gmail.com>
2020-03-15 21:04:03 +0100

    add function to count no. of correct mesurment between sample

--- bin/The cluster of KMTs analysis tools/Tools/Analysis/T_End_Morphology.R
+++ bin/The cluster of KMTs analysis tools/Tools/Analysis/T_End_Morphology.R
@@ -2,8 +2,21 @@
 # The analysis tools to defined end morphology # 
 ################################################
 
+## Count the error rate between sample
+End_Tyep_Error <- function(){
+  if(ncol(Nodes %>% select(starts_with("EndType"))) == 2){
+    Correct <- count(Nodes[,7])
+  End_type_error <- data.frame(Correct = c(round((Correct[2,2]/sum(Correct[2]))*100,2)),
+                               Wrong = c(round((Correct[1,2]/sum(Correct[2]))*100,2)))
+  } else {
+    break
+  }
+  
+  rm(Correct)
+}
+
 ## Count overall number of the end type
-End_Type_overall <- function(){
+End_Type_overall <- function(x){
   
 }
 
