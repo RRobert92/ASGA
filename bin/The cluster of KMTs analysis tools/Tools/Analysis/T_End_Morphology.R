a40ed4eccc3a984605ba6c671d8af225994a91c5
RRobert92 <robert.kiewisz@gmail.com>
2020-03-15 19:58:31 +0100

    Update T_End_Morphology.R

--- bin/The cluster of KMTs analysis tools/Tools/Analysis/T_End_Morphology.R
+++ bin/The cluster of KMTs analysis tools/Tools/Analysis/T_End_Morphology.R
@@ -1,3 +1,13 @@
 ################################################
 # The analysis tools to defined end morphology # 
 ################################################
+
+## Count overall number of the end type
+End_Type_overall <- function(){
+  
+}
+
+## Analyse the end distribution according to the relative position of the KMT end
+End_distribution <- function(){
+  
+}
