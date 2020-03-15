bb33067c28eaa8f55956292831842607dea8b720
RRobert92 <robert.kiewisz@gmail.com>
2020-03-15 22:05:35 +0100

    Add function to count end morphology distribution

--- bin/The cluster of KMTs analysis tools/Tools/Analysis/T_End_Morphology.R
+++ bin/The cluster of KMTs analysis tools/Tools/Analysis/T_End_Morphology.R
@@ -15,12 +15,57 @@ End_Tyep_Error <- function(){
   rm(Correct)
 }
 
-## Count overall number of the end type
-End_Type_overall <- function(x){
-  
-}
-
 ## Analyse the end distribution according to the relative position of the KMT end
-End_distribution <- function(){
+## X is here no. of column in the "Segment"
+## Y is a no. of a Pole 1 or 2
+End_distribution <- function(x, y){
+  ## Function setting
   
-}
+  if(y ==1) {
+    y <- Pole1
+  } else {
+    y <- Pole2
+  }
+  
+  Plus <- data.frame()
+  Minus <- data.frame()
+  
+  ##for x find Node ID that belong to Segment ID
+  for(i in 1:nrow(get(colnames(Segments)[x]))){
+    S_ID <- get(colnames(Segments)[x])[i,1]
+    N_ID_1 <- as.numeric(Segments[S_ID+1, "Node ID #1"])
+    Node_1 <- Nodes[N_ID_1 + 1,]
+    N_ID_2 <- as.numeric(Segments[S_ID+1, "Node ID #2"])
+    Node_2 <- Nodes[N_ID_2 + 1,]
+    
+    if(abs(Node_1["Y Coord"] - y["Y.Coord"]) > abs(Node_2["Y Coord"] - y["Y.Coord"])){
+      Plus[i, 1:8] <- cbind(Node_1,
+                            get(colnames(Segments)[x])[i,"Relative_plus_position"])
+
+      Plus[i, 1] <- x
+      
+      Minus[i, 1:8] <- cbind(Node_2,
+                             get(colnames(Segments)[x])[i,"Relative_minus_position"])
+      
+      Minus[i, 1] <- x
+      
+    } else {
+      Minus[i, 1:8] <- cbind(Node_1,
+                             get(colnames(Segments)[x])[i,"Relative_minus_position"])
+      
+      Minus[i, 1] <- x
+      
+      Plus[i, 1:8] <- cbind(Node_2,
+                           get(colnames(Segments)[x])[i,"Relative_plus_position"])
+      
+      Plus[i, 1] <- x
+      }
+    
+  }
+  names(Plus)[1] <- "Fiber"
+  names(Plus)[8] <- "Relative_plus_position"
+  names(Minus)[1] <- "Fiber"
+  names(Minus)[8] <- "Relative_minus_position"
+  
+  rm(S_ID, N_ID_1, N_ID_2, Node_1, Node_2, y)
+} 
