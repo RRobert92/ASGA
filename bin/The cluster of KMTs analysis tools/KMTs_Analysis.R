d7a72d60d29ecee3773869d03980408cfe386a42
RRobert92 <robert.kiewisz@gmail.com>
2020-03-15 23:36:25 +0100

    Added funcion and packages for end morphology analysis

--- bin/The cluster of KMTs analysis tools/KMTs_Analysis.R
+++ bin/The cluster of KMTs analysis tools/KMTs_Analysis.R
@@ -41,6 +41,7 @@ source("Tools/Analysis/No_of_KMTs_connected_to_the_Pole.R")
 source("Tools/Analysis/No_of_KMTs.R")
 source("Tools/Analysis/T_Inter_Kinetochore_Dist.R")
 source("Tools/Analysis/T_KMT_Curvature.R")
+source("Tools/Analysis/T_End_Morphology.R")
 
 #############
 # Read data #
@@ -72,13 +73,14 @@ close(pb)
 ##   - Inter_Kinetochore_Distance_KMTs_no: [1] Inter-kinetochore distance, [2] KMTs no.
 ##   - Inter_Kinetochore_Distance_KTMs_delta: [1] Inter-kinetochore distance, [2] Delta of KMTs
 ## 3 - KMTs_total_Curvature: [1] Curvature, [2] k0fiber no.
-## 4 - 
+## 4 - Plus/minus_end_morphology_Pole1/2: [1] Fiber, [2 - 3] EndType, [4] EndType Different (if exist), [5] Relative position
 
 if(analysis == 0){
   source("Packages/Analysis/Analyse_No_of_KMTs_Reaching_the_Pole.R")
   source("Packages/Analysis/No_of_KMTs_at_a_kinetochore.R")
   source("Packages/Analysis/P_Inter_Kinetochore_Dist.R")
   source("Packages/Analysis/P_KMT_Curvature.R")
+  source("Packages/Analysis/P_End_Morphology.R")
   
 } else if (analysis == "1"){
   source("Packages/Analysis/Analyse_No_of_KMTs_Reaching_the_Pole.R")
@@ -91,7 +93,7 @@ if(analysis == 0){
   source("Packages/Analysis/P_KMT_Curvature.R")
   
 } else if (analysis == "4"){
-  source("Packages/Analysis/....R")
+  source("Packages/Analysis/P_End_Morphology.R")
   
 } else if (analysis == "5"){
   source("Packages/Analysis/....R")
