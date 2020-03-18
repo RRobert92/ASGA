8da464371da06e781c30fc252ac4d3d6cf9e8033
RRobert92 <robert.kiewisz@gmail.com>
2020-03-18 21:08:39 +0100

    Separated analysis for Pole 1 and 2

--- bin/The cluster of KMTs analysis tools/Packages/Analysis/No_of_KMTs_at_a_kinetochore.R
+++ bin/The cluster of KMTs analysis tools/Packages/Analysis/No_of_KMTs_at_a_kinetochore.R
@@ -3,11 +3,11 @@
 ######################################
 ## The output of this function is a DF with number of KMTs
 
-##########################
+###########################
 # Progress bar for Pole_1 #
 ###########################
 
-total <- as.numeric(ncol(Segments %>% select(starts_with("Pole"))))
+total <- as.numeric(length(which(colnames(Segments) == "Pole1_00") : as.numeric(which(colnames(Segments) == "Pole2_00"))) - 1)
 pb <- winProgressBar(min = 2,
                      max =  total,
                      width = 400)
@@ -16,28 +16,69 @@ pb <- winProgressBar(min = 2,
 # Loop iterating through each KMT for the Pole_1 #
 ##################################################
 
-No_of_KMTs_at_kinetochore <- No_of_KMTs(2)
+No_of_KMTs_at_kinetochore_P1 <- No_of_KMTs(2)
 DF <- data.frame()
-for(i in which(colnames(Segments) == "Pole1_01") : as.numeric(ncol(Segments) - 4)){
+
+for(i in which(colnames(Segments) == "Pole1_00") : as.numeric(which(colnames(Segments) == "Pole2_00") - 1)){
   tryCatch({
     assign("DF",
            No_of_KMTs(i))
     names(DF)[1] <- "No. of KMTs"
-    No_of_KMTs_at_kinetochore <- rbind(No_of_KMTs_at_kinetochore,
-                                        DF)
+    No_of_KMTs_at_kinetochore_P1 <- rbind(No_of_KMTs_at_kinetochore_P1,
+                                          DF)
   },
   error = function(e){})
   Sys.sleep(0.1)
   setWinProgressBar(pb, i, 
-                    title = paste("Counting no. of KMTs at each kinetochore...", 
+                    title = paste("Counting no. of KMTs at each kinetochore from the Pole1...", 
                                   round((i - 1) / total * 100,
                                         0),
                                   "% Done"))
 }
 
-No_of_KMTs_at_kinetochore <- data.frame(No_of_KMTs_at_kinetochore[,1])
-names(No_of_KMTs_at_kinetochore)[1] <- "No. of KMTs"
-write.xlsx(No_of_KMTs_at_kinetochore, paste("Output/", Data_label, "_KMTs_no.xlsx", sep = ""), row.names = FALSE)
+No_of_KMTs_at_kinetochore_P1 <- data.frame(No_of_KMTs_at_kinetochore_P1[,1])
+names(No_of_KMTs_at_kinetochore_P1)[1] <- "No. of KMTs"
+write.xlsx(No_of_KMTs_at_kinetochore_P1, paste("Output/", Data_label, "_KMTs_no_P1.xlsx", sep = ""), row.names = FALSE)
+
+rm(DF)
+
+###########################
+# Progress bar for Pole_2 #
+###########################
+
+total <- which(colnames(Segments) == colnames(Segments %>% select(starts_with("Pole")))[ncol(Segments %>% select(starts_with("Pole")))]) - as.numeric(which(colnames(Segments) == "Pole2_00") - 1)
+pb <- winProgressBar(min = 0,
+                     max =  total,
+                     width = 400)
+
+###################################################
+# Loop iterating through each KMT for the Pole_12 #
+###################################################
+
+No_of_KMTs_at_kinetochore_P2 <- No_of_KMTs(2)
+DF <- data.frame()
+
+for(i in which(colnames(Segments) == "Pole2_00") : as.numeric(ncol(Segments) - 4)){
+  tryCatch({
+    assign("DF",
+           No_of_KMTs(i))
+    names(DF)[1] <- "No. of KMTs"
+    No_of_KMTs_at_kinetochore_P2 <- rbind(No_of_KMTs_at_kinetochore_P2,
+                                          DF)
+  },
+  error = function(e){})
+  
+  Sys.sleep(0.1)
+  setWinProgressBar(pb, i - as.numeric(which(colnames(Segments) == "Pole2_00")), 
+                    title = paste("Counting no. of KMTs at each kinetochore from the Pole2...",
+                                  round((i - as.numeric(which(colnames(Segments) == "Pole2_00") - 1)) / total * 100,
+                                        0), 
+                                  "% Done"))
+}
+
+No_of_KMTs_at_kinetochore_P2 <- data.frame(No_of_KMTs_at_kinetochore_P2[,1])
+names(No_of_KMTs_at_kinetochore_P2)[1] <- "No. of KMTs"
+write.xlsx(No_of_KMTs_at_kinetochore_P2, paste("Output/", Data_label, "_KMTs_no_P2.xlsx", sep = ""), row.names = FALSE)
 
 rm(DF)
 
