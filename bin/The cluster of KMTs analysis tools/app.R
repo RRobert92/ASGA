097b1b2e649547a46ec8d677d79acd568ede4acc
RRobert92 <robert.kiewisz@gmail.com>
2020-03-19 14:36:43 +0100

    Update app.R

--- bin/The cluster of KMTs analysis tools/app.R
+++ bin/The cluster of KMTs analysis tools/app.R
@@ -4,9 +4,9 @@
 
 source("Utility/Library.R")
 
-################################
-# Defin no. of data to analyse #
-################################
+#######################################################
+# Defin no. of data for the pre-analysis and analysis #
+#######################################################
 
 No_of_Data <- dlg_input("How much data you want to analyzed and compared.",
                           1)$res
@@ -18,9 +18,9 @@ for(n in 1:No_of_Data){
 }
 rm(list=setdiff(ls(), list("Data_label", "No_of_Data")))
 
-##########################
-# Load all analysed Data #
-##########################
+###############################
+# Back load all analysed Data #
+###############################
 Data_label <- as.data.frame(str_split(Data_label, "_"))
 Data_label <- as.character(Data_label[1,1])
 
@@ -30,9 +30,9 @@ source("Utility/Load_Analysis.R")
 ## Prepare data depending from specied need: bin all for P1 & P2 or analysed P1 & P2 separetly 
 source("Packages/Post_Analysis/Data_Praparation.R")
 
-##############
-# Plots Data #
-##############
+###################
+# Post - Analysis #
+###################
 source("Tools/Post_Analysis/Plots/PL_Length_Distirbution.R")
 source("Tools/Post_Analysis/Plots/PL_Inter-Kinetochore_Distance.R")
 source("Tools/Post_Analysis/Plots/PL_KMTs_at_the_Pole.R")
