46d2d00b44893000b052dd46993f1f12c8c4849e
RRobert92 <robert.kiewisz@gmail.com>
2020-03-23 14:50:04 +0100

    Update PL_Length_Distirbution.R

--- bin/The cluster of KMTs analysis tools/Tools/Post_Analysis/Plots/PL_Length_Distirbution.R
+++ bin/The cluster of KMTs analysis tools/Tools/Post_Analysis/Plots/PL_Length_Distirbution.R
@@ -1,29 +1,24 @@
-LD_0_1.5 <- data.frame(LD[with(LD, plus_dist_to_kinetochore_core <= 1.5 & plus_dist_to_kinetochore_core > 0),][,1],
-                       LD[with(LD, plus_dist_to_kinetochore_core <= 1.5 & plus_dist_to_kinetochore_core > 0),][,2])
-
-P <- ggplot(LD_0_1.5, aes(KMTs.length)) + geom_density(colour = "red", size = 1) + ylim(0, 1) + xlim(0, 10) + theme_classic() +
-  ggtitle(paste(Data_label, "Length Distribution")) +
-  xlab("KMTs length (um)") + 
-  ylab("Normalized no. of KMTs")
-
-LD_1.5_3 <- data.frame(LD[with(LD, plus_dist_to_kinetochore_core <= 3 & plus_dist_to_kinetochore_core > 1.5),][,1],
-                       LD[with(LD, plus_dist_to_kinetochore_core <= 3 & plus_dist_to_kinetochore_core > 1.5),][,2])
-P <- P + geom_density(data = LD_1.5_3, aes(KMTs.length), colour = "blue", size = 1) + theme_classic()
-
-LD_3_4.5 <- data.frame(LD[with(LD, plus_dist_to_kinetochore_core <= 10 & plus_dist_to_kinetochore_core > 3),][,1],
-                       LD[with(LD, plus_dist_to_kinetochore_core <= 10 & plus_dist_to_kinetochore_core > 3),][,2])
-P <- P + geom_density(data = LD_3_4.5, aes(KMTs.length), colour = "green", size = 1) + theme_classic()
-
-print(P)
-
-print(ggplot(LD, aes(`KMTs length`)) +
-        geom_density(colour = "black", size = 1) + 
-        ylim(0, 1) + xlim(0, 10) + theme_classic() + 
-        ggtitle(paste(Data_label, "Length Distribution")) +
-        xlab("KMTs length (um)") + 
-        ylab("Normalized no. of KMTs"))
-
-write.xlsx(LD, paste("Output/", Data_label, "_LD.xlsx", sep = ""))
-write.xlsx(LD_0_1.5, paste("Output/", Data_label, "_LD_0_1.5.xlsx", sep = ""))
-write.xlsx(LD_1.5_3, paste("Output/", Data_label, "_LD_1.5_3.xlsx", sep = ""))
-write.xlsx(LD_3_4.5, paste("Output/", Data_label, "_LD_3_4.5.xlsx", sep = ""))
+for(i in 1:No_of_Data){
+  LD_0_1.5 <- data.frame(get(paste(Data_label, "_", i, "_LD", sep = ""))[with(get(paste(Data_label, "_", i, "_LD", sep = "")), 
+                                                                              plus_dist_to_kinetochore_core <= 1.5 & plus_dist_to_kinetochore_core > 0),][,1],
+                         get(paste(Data_label, "_", i, "_LD", sep = ""))[with(get(paste(Data_label, "_", i, "_LD", sep = "")), 
+                                                                              plus_dist_to_kinetochore_core <= 1.5 & plus_dist_to_kinetochore_core > 0),][,2])
+  LD_1.5_3 <- data.frame(get(paste(Data_label, "_", i, "_LD", sep = ""))[with(get(paste(Data_label, "_", i, "_LD", sep = "")), 
+                                                                              plus_dist_to_kinetochore_core <= 3 & plus_dist_to_kinetochore_core > 1.5),][,1],
+                         get(paste(Data_label, "_", i, "_LD", sep = ""))[with(get(paste(Data_label, "_", i, "_LD", sep = "")),
+                                                                              plus_dist_to_kinetochore_core <= 3 & plus_dist_to_kinetochore_core > 1.5),][,2])
+  LD_3_4.5 <- data.frame(get(paste(Data_label, "_", i, "_LD", sep = ""))[with(get(paste(Data_label, "_", i, "_LD", sep = "")), 
+                                                                              plus_dist_to_kinetochore_core <= 10 & plus_dist_to_kinetochore_core > 3),][,1],
+                         get(paste(Data_label, "_", i, "_LD", sep = ""))[with(get(paste(Data_label, "_", i, "_LD", sep = "")), 
+                                                                              plus_dist_to_kinetochore_core <= 10 & plus_dist_to_kinetochore_core > 3),][,2])
+  write.xlsx(LD_0_1.5, paste("Output/", Data_label, "_", i, "_LD_0_1.5.xlsx", sep = ""))
+  write.xlsx(LD_1.5_3, paste("Output/", Data_label, "_", i, "_LD_1.5_3.xlsx", sep = ""))
+  write.xlsx(LD_3_4.5, paste("Output/", Data_label, "_", i, "_LD_3_4.5.xlsx", sep = ""))
+  
+  P <- ggplot(LD_0_1.5, aes("1", KMTs.length)) + geom_violin(fill = "dodgerblue4") + geom_jitter(width = 0.2) + theme_classic()
+  P <- P + geom_violin(data = LD_1.5_3, aes("2", KMTs.length), fill = "olivedrab4") + geom_jitter(data = LD_1.5_3, aes("2", KMTs.length), width = 0.2)
+  P <- P + geom_violin(data = LD_3_4.5, aes("3", KMTs.length), fill = "firebrick4") + geom_jitter(data = LD_3_4.5, aes("3", KMTs.length), width = 0.2)
+  print(P)
+  
+  
+}
