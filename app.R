641d044e152b2ba93148cd3d2c67eaced27843f8
RRobert92 <robert.kiewisz@gmail.com>
2020-03-28 13:03:27 +0100

    Clened old code

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # Library (load & install if missing) #
 #######################################
 
-source("Utility/Library.R")
+source("bin/Utility/Library.R")
 
 #######################################################
 # Defin no. of data for the pre-analysis and analysis #
@@ -14,7 +14,7 @@ No_of_Data <- as.numeric(No_of_Data)
 for(n in 1:No_of_Data){
   rm(list = setdiff(ls(), list("Data_label", "No_of_Data")))
   
-  source("Utility/KMTs_Analysis.R")
+  source("bin/Utility/KMTs_Analysis.R")
 }
 rm(list=setdiff(ls(), list("Data_label", "No_of_Data")))
 
@@ -25,17 +25,17 @@ Data_label <- as.data.frame(str_split(Data_label, "_"))
 Data_label <- as.character(Data_label[1,1])
 
 ## Load data from the soucre location Output/...
-source("Utility/Load_Analysis.R")
+source("bin/Utility/Load_Analysis.R")
 
 ## Prepare data depending from specied need: bin all for P1 & P2 or analysed P1 & P2 separetly 
-source("Packages/Post_Analysis/Data_Praparation.R")
+source("bin/Packages/Post_Analysis/Data_Praparation.R")
 
 ###################
 # Post - Analysis #
 ###################
-source("Tools/Post_Analysis/Plots/PL_Length_Distirbution.R")
-source("Tools/Post_Analysis/Plots/PL_Inter-Kinetochore_Distance.R")
-source("Tools/Post_Analysis/Plots/PL_KMTs_at_the_Pole.R")
-source("Tools/Post_Analysis/Plots/PL_(-)_End_Distribution.R")
-source("Tools/Post_Analysis/Plots/PL_Total_Curvature.R")
-source("Tools/Post_Analysis/Plots/PL_Local_Curvature.R")
+source("bin/Tools/Post_Analysis/Plots/PL_Length_Distirbution.R")
+source("bin/Tools/Post_Analysis/Plots/PL_Inter-Kinetochore_Distance.R")
+source("bin/Tools/Post_Analysis/Plots/PL_KMTs_at_the_Pole.R")
+source("bin/Tools/Post_Analysis/Plots/PL_(-)_End_Distribution.R")
+source("bin/Tools/Post_Analysis/Plots/PL_Total_Curvature.R")
+source("bin/Tools/Post_Analysis/Plots/PL_Local_Curvature.R")
