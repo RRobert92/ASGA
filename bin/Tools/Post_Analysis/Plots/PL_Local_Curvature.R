3338cdc8746adcf265f9dc86961200c92f198533
RRobert92 <robert.kiewisz@gmail.com>
2020-03-30 22:18:49 +0200

    Update PL_Local_Curvature.R

--- bin/Tools/Post_Analysis/Plots/PL_Local_Curvature.R
+++ bin/Tools/Post_Analysis/Plots/PL_Local_Curvature.R
@@ -1,60 +1,118 @@
-local_full <- data.frame()
-local_full[1,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 1.0 & Relative_Position > 0.899),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 1.0 & Relative_Position > 0.899),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 1.0 & Relative_Position > 0.899),][,1])))
+for(i in 1:No_of_Data){
+  LD100max <- max(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[6])
+  LD100min <- min(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[6])
+  LD100 <- LD100max
+  LD80 <- ((80*(LD100max-LD100min))+(100*LD100min))/100
+  LD45 <- ((45*(LD100max-LD100min))+(100*LD100min))/100
+  
+  LD_45 <- data.frame(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")),
+                                                                                        End_to_Pole <= LD45 & End_to_Pole > 0),][,1],
+                      get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                        End_to_Pole <= LD45 & End_to_Pole > 0),][,2])
+  LD_80 <- data.frame(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                        End_to_Pole <= LD80 & End_to_Pole > LD45),][,1],
+                      get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")),
+                                                                                        End_to_Pole <= LD80 & End_to_Pole > LD45),][,2])
+  LD_100 <- data.frame(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                         End_to_Pole <= LD100 & End_to_Pole > LD80),][,1],
+                       get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                         End_to_Pole <= LD100 & End_to_Pole > LD80),][,2])
+  
+  local_full <- data.frame()
+  n = 1
+  for(j in 1:11){
+    local_full[j,1:3] <- data.frame(Curvature = median(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]),
+                                    Relative_Position = median(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,2]),
+                                    SD_error = sd(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]) / 
+                                      sqrt(length(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1])))
+    n = n - 0.1
+  }
+  LD_45_median <- local_full
+  
+  local_full <- data.frame()
+  n = 1
+  for(j in 1:11){
+    local_full[j,1:3] <- data.frame(Curvature = median(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]),
+                                    Relative_Position = median(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,2]),
+                                    SD_error = sd(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]) / 
+                                      sqrt(length(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1])))
+    n = n - 0.1
+  }
+  LD_80_median <- local_full
+  
+  local_full <- data.frame()
+  n = 1
+  for(j in 1:11){
+    local_full[j,1:3] <- data.frame(Curvature = median(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]),
+                                    Relative_Position = median(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,2]),
+                                    SD_error = sd(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]) / 
+                                      sqrt(length(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1])))
+    n = n - 0.1
+  }
+  LD_100_median <- local_full
+  
+  
+  
+  write.xlsx(LD_45, paste("bin/Output/", Data_label, "_", i, "_LC_Pole_45.xlsx" ,sep = ""))
+  write.xlsx(LD_45_median, paste("bin/Output/", Data_label, "_", i, "_LC_Pole_m_45.xlsx" ,sep = ""))
+  write.xlsx(LD_80, paste("bin/Output/", Data_label, "_", i, "_LC_Pole_80.xlsx" ,sep = ""))
+  write.xlsx(LD_80_median, paste("bin/Output/", Data_label, "_", i, "_LC_Pole_m_80.xlsx" ,sep = ""))
+  write.xlsx(LD_100, paste("bin/Output/", Data_label, "_", i, "_LC_Pole_100.xlsx" ,sep = ""))
+  write.xlsx(LD_100_median, paste("bin/Output/", Data_label, "_", i, "_LC_Pole_m_100.xlsx" ,sep = ""))
+}
 
-local_full[2,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.9 & Relative_Position > 0.799),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.9 & Relative_Position > 0.799),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.9 & Relative_Position > 0.799),][,1])))
 
-local_full[3,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.8 & Relative_Position > 0.699),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.8 & Relative_Position > 0.699),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.8 & Relative_Position > 0.699),][,1])))
-
-local_full[4,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.7 & Relative_Position > 0.599),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.7 & Relative_Position > 0.599),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.7 & Relative_Position > 0.599),][,1])))
-
-local_full[5,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.6 & Relative_Position > 0.499),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.6 & Relative_Position > 0.499),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.6 & Relative_Position > 0.499),][,1])))
-
-local_full[6,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.5 & Relative_Position > 0.399),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.5 & Relative_Position > 0.399),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.5 & Relative_Position > 0.399),][,1])))
-
-local_full[7,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.4 & Relative_Position > 0.299),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.4 & Relative_Position > 0.299),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.4 & Relative_Position > 0.299),][,1])))
-
-local_full[8,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.3 & Relative_Position > 0.199),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.3 & Relative_Position > 0.199),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.3 & Relative_Position > 0.199),][,1])))
-
-local_full[9,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.2 & Relative_Position > 0.099),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.2 & Relative_Position > 0.099),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.2 & Relative_Position > 0.099),][,1])))
-
-local_full[9,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.1 & Relative_Position > 0.000),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.1 & Relative_Position > 0.000),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.1 & Relative_Position > 0.000),][,1])))
-
-local_full[10,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.0 & Relative_Position > -0.099),][,1])),
-                             To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.0 & Relative_Position > -0.099),][,2])),
-                             SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < 0.0 & Relative_Position > -0.099),][,1])))
-
-local_full[11,1:3] <- data.frame(To_1.0_c = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < -0.1 & Relative_Position > -0.199),][,1])),
-                              To_1.0_p = median(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < -0.1 & Relative_Position > -0.199),][,2])),
-                              SD = sd(unlist(Local_Curvature[with(Local_Curvature, Relative_Position < -0.1 & Relative_Position > -0.199),][,1])))
-
-print(ggplot(local_full, aes(To_1.0_p, To_1.0_c)) + geom_point() + ylim(1, 1.01) + xlim(-0.3, 1))
-
-PL <- ggplot(Local_Curvature, aes(Relative_Position, Curvature)) + 
-  geom_smooth(size = 1, method = "gam") +
-  xlim(-0.3, 1) + ylim(1, 1.03) +
-  theme_classic()
-PL <- PL + geom_point(data = local_full, aes(To_1.0_p, To_1.0_c))
-print(PL)
-
-write.xlsx(Local_Curvature, paste("Output/", Data_label, "_Local_Curvature.xlsx", sep = ""))
-write.xlsx(local_full, paste("Output/", Data_label, "_Local_C_bin.xlsx", sep = ""))
+for(i in 1:No_of_Data){
+  LD_45 <- data.frame(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                        Elipse_Position == "25%" ),][,1],
+                      get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                        Elipse_Position == "25%"),][,2])
+  LD_80 <- data.frame(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                        Elipse_Position == "50%"),][,1],
+                      get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")),
+                                                                                        Elipse_Position == "50%"),][,2])
+  LD_100 <- data.frame(get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                         Elipse_Position == "100%"),][,1],
+                       get(paste(Data_label, "_", i, "_Local_Curvature", sep = ""))[with(get(paste(Data_label, "_", i, "_Local_Curvature", sep = "")), 
+                                                                                         Elipse_Position == "100%"),][,2])
+  
+  local_full <- data.frame()
+  n = 1
+  for(j in 1:11){
+    local_full[j,1:3] <- data.frame(Curvature = median(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]),
+                                    Relative_Position = median(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,2]),
+                                    SD_error = sd(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]) / 
+                                      sqrt(length(LD_45[with(LD_45, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1])))
+    n = n - 0.1
+  }
+  LD_45_median <- local_full
+  
+  local_full <- data.frame()
+  n = 1
+  for(j in 1:11){
+    local_full[j,1:3] <- data.frame(Curvature = median(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]),
+                                    Relative_Position = median(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,2]),
+                                    SD_error = sd(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]) / 
+                                      sqrt(length(LD_80[with(LD_80, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1])))
+    n = n - 0.1
+  }
+  LD_80_median <- local_full
+  
+  local_full <- data.frame()
+  n = 1
+  for(j in 1:11){
+    local_full[j,1:3] <- data.frame(Curvature = median(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]),
+                                    Relative_Position = median(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,2]),
+                                    SD_error = sd(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1]) / 
+                                      sqrt(length(LD_100[with(LD_100, Relative_Position < n & Relative_Position > as.numeric(n-0.101)),][,1])))
+    n = n - 0.1
+  }
+  LD_100_median <- local_full
+  
+  write.xlsx(LD_45, paste("bin/Output/", Data_label, "_", i, "_LC_E_45.xlsx" ,sep = ""))
+  write.xlsx(LD_45_median, paste("bin/Output/", Data_label, "_", i, "_LC_E_m_45.xlsx" ,sep = ""))
+  write.xlsx(LD_80, paste("bin/Output/", Data_label, "_", i, "_LC_E_80.xlsx" ,sep = ""))
+  write.xlsx(LD_80_median, paste("bin/Output/", Data_label, "_", i, "_LC_E_m_80.xlsx" ,sep = ""))
+  write.xlsx(LD_100, paste("bin/Output/", Data_label, "_", i, "_LC_E_100.xlsx" ,sep = ""))
+  write.xlsx(LD_100_median, paste("bin/Output/", Data_label, "_", i, "_LC_E_m_100.xlsx" ,sep = ""))
+}
