d51d6c12f1ffbb399a2e79846cd59fd6f833b0be
RRobert92 <robert.kiewisz@gmail.com>
2020-03-31 14:05:20 +0200

    Update for the fibera area function

--- bin/Packages/Analysis/P_Fiber_Area.R
+++ bin/Packages/Analysis/P_Fiber_Area.R
@@ -7,9 +7,41 @@ for (i in which(colnames(Segments) == "Pole1_00"):as.numeric(which(colnames(Segm
   assign(paste(colnames(Segments)[i]), 
          leading_KMTsv2(i, Pole1))
 }
+for (i in which(colnames(Segments) == "Pole1_00"):as.numeric(which(colnames(Segments) == "Pole2_00") - 1)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         Leadig_Points(i))
+}
+for (i in which(colnames(Segments) == "Pole1_00"):as.numeric(which(colnames(Segments) == "Pole2_00") - 1)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         find_polygon(i))
+}
+for (i in which(colnames(Segments) == "Pole1_00"):as.numeric(which(colnames(Segments) == "Pole2_00") - 1)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         duplicated_points(i))
+}
+for (i in which(colnames(Segments) == "Pole1_00"):as.numeric(which(colnames(Segments) == "Pole2_00") - 1)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         median_point(i))
+}
 
 ## Find leading KMTs in the fiber for Pole2
 for (i in as.numeric(which(colnames(Segments) == "Pole2_00")):as.numeric(ncol(Segments) - 4)) {
   assign(paste(colnames(Segments)[i]), 
          leading_KMTsv2(i, Pole2))
+}
+for (i in as.numeric(which(colnames(Segments) == "Pole2_00")):as.numeric(ncol(Segments) - 4)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         Leadig_Points(i))
+}
+for (i in as.numeric(which(colnames(Segments) == "Pole2_00")):as.numeric(ncol(Segments) - 4)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         find_polygon(i))
+}
+for (i in as.numeric(which(colnames(Segments) == "Pole2_00")):as.numeric(ncol(Segments) - 4)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         duplicated_points(i))
+}
+for (i in as.numeric(which(colnames(Segments) == "Pole2_00")):as.numeric(ncol(Segments) - 4)) {
+  assign(paste(colnames(Segments)[i], "fiber", sep = "_"), 
+         median_point(i))
 }
--- bin/Tools/Analysis/T_Fiber_Area.R
+++ bin/Tools/Analysis/T_Fiber_Area.R
@@ -1,8 +1,9 @@
 ##################################################################
 # Set of functions to define fiber area and neighborhood density # 
 ##################################################################
-s
 ## Calculate ratio of KMT_length / (-) end distance to the pole. Lower ratio, define leading KMTs
+## x is an number of the column with the label name
+## Y is name of the pole Pole1 or Pole2
 
 leading_KMTsv2 <- function(x, y) {
   j = 1
@@ -24,13 +25,104 @@ leading_KMTsv2 <- function(x, y) {
   bind_cols(get(paste(colnames(Segments)[x])), leading)
 }
 
-##find point for lading KMTS, for i = i+5 == 0.5um
+##find point for lading KMTS, for j = j+5 == 0.5um
 Leadig_Points <- function(x) {
-  i = 1
+  j = 1
   leading_points <- data.frame(Leading_ID = as.numeric())
-  while (i <= nrow(get(paste(colnames(Segments)[x], which.max(as.matrix(get(colnames(Segments)[x])["Leading"])), sep = "_")))) {
-    leading_points[i, ] <- get(paste(colnames(Segments)[x], which.max(as.matrix(get(colnames(Segments)[x])["Leading"])), sep = "_"))[i, 1]
-    i = i + 5
+  while (j <= nrow(get(paste(colnames(Segments)[x], which.max(as.matrix(get(colnames(Segments)[x])["Leading"])), sep = "_")))) {
+    leading_points[j, ] <- get(paste(colnames(Segments)[x], which.max(as.matrix(get(colnames(Segments)[x])["Leading"])), sep = "_"))[j, 1]
+    j = j + 24
   }
   leading_points <- na.omit(leading_points)
-}ss
+}
+
+##find all points which corespond to the slice of the fiber
+find_polygon <- function(x) {
+  i = 1
+  lead_points_id <- data.frame(Distance = as.numeric())
+  Distance <- data.frame(V1 = as.numeric())
+  
+  while (i <= as.numeric(nrow(get(paste(colnames(Segments)[x], "fiber", sep = "_"))))) {
+    lead_points_id <- data.frame(X_lead = Points[as.numeric(get(paste(colnames(Segments)[x], "fiber", sep = "_")) [i, ] + 1), 2],
+                                 Y_lead = Points[as.numeric(get(paste(colnames(Segments)[x], "fiber", sep = "_")) [i, ] + 1), 3],
+                                 Z_lead = Points[as.numeric(get(paste(colnames(Segments)[x], "fiber", sep = "_")) [i, ] + 1), 4])
+    j = 1
+    lead_points_id_full <- data.frame(t = as.numeric())
+    
+    for (j in 1:as.numeric(nrow(get(paste(colnames(Segments)[x]))))) {
+      lead_points_id_full <- apply(lead_points_id, MARGIN = 2,
+                                   function(y) rep(y, as.numeric(nrow(get(paste(colnames(Segments)[x], j, sep = "_"))))))
+      
+      lead_points_id_full <- cbind(get(paste(colnames(Segments)[x], j, sep = "_"))[1], lead_points_id,
+                                   get(paste(colnames(Segments)[x], j, sep = "_"))[2:4])
+      
+      lead_points_id_full$distance <- apply(lead_points_id_full[2:7], 1, 
+                                            function(x) dist(matrix(x, 
+                                                                    nrow = 2, 
+                                                                    byrow = TRUE)))
+      
+      Distance[i, j] <- lead_points_id_full[as.numeric(which.min(lead_points_id_full$distance)), 1]
+      lead_points_id_full <- data.frame(t = as.numeric())
+    }
+    i = i + 1
+  }
+  bind_cols(get(paste(colnames(Segments)[x], "fiber", sep = "_")), Distance)
+}
+
+##remove duplicated points
+duplicated_points <- function(x){
+  DF <- get(paste(colnames(Segments)[x], "fiber",  sep = "_"))
+  
+  for(i in 1:ncol(get(paste(colnames(Segments)[x], "fiber", sep = "_")))){
+    DF[,i][duplicated(DF[,i])] <- NA
+  }
+  ##check if there is no hole in dataset if yes remove 
+  for(i in 1:ncol(get(paste(colnames(Segments)[x], "fiber", sep = "_")))){
+    for(j in 2:nrow(get(paste(colnames(Segments)[x], "fiber", sep = "_")))){
+      if(is.na(DF[j-1,i]) && is.na(DF[j+2,i])){
+        DF[j:nrow(get(paste(colnames(Segments)[x], "fiber", sep = "_"))),i] <- NA
+      }
+      else{}
+    }
+  }
+  DF
+}
+
+##get a median point for each positin and put cbin in first col
+median_point <- function(x){
+  ##for looop to creat df of x y z coord for eahc position
+  ##mediana of x y z coord
+  ## writ it in a table
+  ##cbin with Pole1_00_fiber
+  Median_id <- data.frame(X_Coord = as.numeric(),
+                          Y_Coord = as.numeric(),
+                          Z_Coord = as.numeric())
+  
+  for (i in 1:as.numeric(nrow(get(paste(colnames(Segments)[x], "fiber", sep = "_"))))) {
+    DF <- data.frame(X_Coord = as.numeric(),
+                     Y_Coord = as.numeric(),
+                     Z_Coord = as.numeric())
+    
+    for (j in 1:as.numeric(ncol(get(paste(colnames(Segments)[x], "fiber", sep = "_"))))) {
+      DF[j,1] <- Points[as.numeric(get(paste(colnames(Segments)[x], "fiber", sep = "_"))[i,j]+1),2]
+      DF[j,2] <- Points[as.numeric(get(paste(colnames(Segments)[x], "fiber", sep = "_"))[i,j]+1),3]
+      DF[j,3] <- Points[as.numeric(get(paste(colnames(Segments)[x], "fiber", sep = "_"))[i,j]+1),4]
+    }
+    
+    Median_id[i,1] <- median(na.omit(DF$X_Coord))
+    Median_id[i,2] <- median(na.omit(DF$Y_Coord))
+    Median_id[i,3] <- median(na.omit(DF$Z_Coord))
+  }
+  
+  DF <- cbind(Median_id, get(paste(colnames(Segments)[x], "fiber", sep = "_")))
+  for (i in 1:nrow(DF)){
+    if(sum(na.omit(colSums(DF[i, which(colnames(DF) == "V1"):ncol(DF)] != 0))) < 3){
+      DF[i,1:12] <- NA
+    } else {}
+  }
+  if (sum(which(is.na(DF[1]))) != 0){
+    DF[-c(which(is.na(DF[1]))),]
+  } else {
+    DF
+  }
+}
