78f18f6f6f240651b2253d07f3ffaa0c612a358c
RRobert92 <robert.kiewisz@gmail.com>
2020-04-17 21:51:30 +0200

    Update T_KMT_Minus_End_Seeds.R

--- bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
+++ bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
@@ -9,33 +9,76 @@
 # Interactor_ID, 
 # class of interactor, 
 # p to P distance
-# Position at the spindle pole axis a.k.a. pole to pole axis,
 # Relative position,
 # Plus end distance to the pole,
 # Ellipse position
 Minus_end_seed <- function(x){
+  Minus_end <- data.frame()
   for(i in 1:nrow(get(paste(colnames(Segments)[x])))){
     DF <- data.frame()
-    for(j in 1:nrow(get(paste(colnames(Segments)[x], j, sep = "_")))){
+    for(j in 1:nrow(get(paste(colnames(Segments)[x], i, sep = "_")))){
       p_to_P <- Nodes
-      p_to_P[5:7] <- get(paste(colnames(Segments)[x],j, sep = "_"))[j,2:4] 
+      p_to_P[5:7] <- get(paste(colnames(Segments)[x],i, sep = "_"))[j,2:4] 
       
       p_to_P$dist <- apply(p_to_P[2:7], 
-                       1, 
-                       function(y) dist(matrix(y, 
-                                               nrow = 2, 
-                                               byrow = TRUE)))
-      DF <- data.frame(p_to_P[with(p_to_P, dist <= 0.1 & dist >= 0),"Node ID"],
-                       p_to_P[with(p_to_P, dist <= 0.1 & dist >= 0), "dist"])
-      for (i in 1:nrow(DF)){
-        all_end <- Segments %>% filter_at(vars(starts_with("Node")),
-                                          any_vars(. == DF[i,1]))
-        defin_end[i,1:3] <- all_end %>% select(`Segment ID`,
-                                               `Node ID #1`,
-                                               `Node ID #2`)
+                           1, 
+                           function(y) dist(matrix(y, 
+                                                   nrow = 2, 
+                                                   byrow = TRUE)))
+      DF <- data.frame(p_to_P[with(p_to_P, dist <= minus_distance & dist >= 0),"Node ID"],
+                       p_to_P[with(p_to_P, dist <= minus_distance & dist >= 0), "dist"])
+      if(nrow(DF) > 0){
+        all_end <- data.frame()
+        defin_end <- data.frame()
+        for (k in 1:nrow(DF)){
+          all_end <- Segments %>% filter_at(vars(starts_with("Node")),
+                                            any_vars(. == DF[k,1]))
+          defin_end[k,1:3] <- all_end %>% select(`Segment ID`,
+                                                 `Node ID #1`,
+                                                 `Node ID #2`)
+          
+          if(defin_end[1,1] %in% Segments_SMT$`Segment ID`){
+            defin_end[k,4] <-"SMT"
+          } else {
+            defin_end[k,4] <-"KMT"
+          }
+          
+          if(defin_end[k,1] == get(paste(colnames(Segments)[x]))[i,1]){
+            defin_end[k,1:4] <- NA
+            DF[k,1:2] <- NA
+          }
+        }
+        defin_end <- na.omit(defin_end)
+        DF <- na.omit(DF)
+        if(nrow(DF) > 0){
+          defin_end <- cbind(paste(colnames(Segments)[x], i, sep = "_"),
+                             defin_end[1],
+                             defin_end[4],
+                             round(DF[2],4),
+                             get(paste(colnames(Segments)[x], i, sep = "_"))[j,5],
+                             get(paste(colnames(Segments)[x]))[1,5],
+                             get(paste(colnames(Segments)[x]))[1,6])
+          names(defin_end)[1] <- "KMT_ID"
+          names(defin_end)[2] <- "Interactor_ID"
+          names(defin_end)[3] <- "I_class"
+          names(defin_end)[4] <- "p_to_P_dist"
+          names(defin_end)[5] <- "Relative_pos"
+          names(defin_end)[6] <- "Plus_end_dist"
+          names(defin_end)[7] <- "Ellipse"
+          
+          Minus_end <- rbind(Minus_end,
+                             defin_end)
+        }
+        
       }
-      
     }
+    list <- unique(Minus_end$Interactor_ID)
+    Temp <- data.frame()
+    for(k in list){
+      DF <- Minus_end %>% filter_at(vars("Interactor_ID"),
+                              any_vars(. == k))
+      Temp[k,1:7] <- DF[which.min(DF$p_to_P_dist), 1:7]
+    }
+    Minus_end <- na.omit(Temp)
   }
-  
 }
