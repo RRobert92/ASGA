0a0c39d283f4b4e3cd3564136acf2659ff241ccf
RRobert92 <robert.kiewisz@gmail.com>
2020-04-18 18:33:35 +0200

    update description

--- bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
+++ bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
@@ -2,7 +2,7 @@
 # Function to calculate distribution of a (-) end in close proximity to the KMT #
 ##################################################################################
 # The tool is calculating distance of each point on KMT (p) to every (-) end of SMT and KMT (P_S or P_K)
-# Then we associate point_id to segment_id of P_S or P_K which are closer to the p then 55nm 
+# Then we associate point_id to segment_id of P_S or P_K which are closer to the p then 100nm 
 # Multiple same segment_ID are marred to single entrance with their closes distance to the p
 # The final output is data file for each KMT with: 
 # KMT_ID, 
@@ -12,6 +12,7 @@
 # Relative position,
 # Plus end distance to the pole,
 # Ellipse position
+
 Minus_end_seed <- function(x){
   Minus_end <- data.frame()
   for(i in 1:nrow(get(paste(colnames(Segments)[x])))){
