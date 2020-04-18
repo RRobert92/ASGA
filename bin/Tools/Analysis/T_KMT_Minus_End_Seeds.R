d9e2399b286f3fd85f84d967bb364419b2dc1a75
RRobert92 <robert.kiewisz@gmail.com>
2020-04-18 17:54:39 +0200

    Added selection of only (-) ends

--- bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
+++ bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
@@ -31,6 +31,7 @@ Minus_end_seed <- function(x){
       if(nrow(DF) > 0){
         all_end <- data.frame()
         defin_end <- data.frame()
+        
         for (k in 1:nrow(DF)){
           all_end <- Segments %>% filter_at(vars(starts_with("Node")),
                                             any_vars(. == DF[k,1]))
@@ -51,6 +52,60 @@ Minus_end_seed <- function(x){
         }
         defin_end <- na.omit(defin_end)
         DF <- na.omit(DF)
+        
+        if(nrow(DF) > 0){
+          end_type <- data.frame()
+          for(k in 1:nrow(defin_end)){
+            N1_to_pole1 <- sqrt((Pole1[1,1] - Nodes[as.numeric(defin_end[k,2]+1),2])^2 + 
+                                (Pole1[1,2] - Nodes[as.numeric(defin_end[k,2]+1),3])^2 + 
+                                (Pole1[1,3] - Nodes[as.numeric(defin_end[k,2]+1),4])^2)
+            N1_to_pole2 <- sqrt((Pole2[1,1] - Nodes[as.numeric(defin_end[k,2]+1),2])^2 + 
+                                (Pole2[1,2] - Nodes[as.numeric(defin_end[k,2]+1),3])^2 + 
+                                (Pole2[1,3] - Nodes[as.numeric(defin_end[k,2]+1),4])^2)
+            N2_to_pole1 <- sqrt((Pole1[1,1] - Nodes[as.numeric(defin_end[k,3]+1),2])^2 + 
+                                (Pole1[1,2] - Nodes[as.numeric(defin_end[k,3]+1),3])^2 + 
+                                (Pole1[1,3] - Nodes[as.numeric(defin_end[k,3]+1),4])^2)
+            N2_to_pole2 <- sqrt((Pole2[1,1] - Nodes[as.numeric(defin_end[k,3]+1),2])^2 + 
+                                (Pole2[1,2] - Nodes[as.numeric(defin_end[k,3]+1),3])^2 + 
+                                (Pole2[1,3] - Nodes[as.numeric(defin_end[k,3]+1),4])^2)
+            Node_to_Pole <- rbind(N1_to_pole1,
+                                  N1_to_pole2,
+                                  N2_to_pole1,
+                                  N2_to_pole2)
+            if(which.min(as.matrix(Node_to_Pole)) == 1 && which.max(as.matrix(Node_to_Pole)) == 2){
+              end_type[k,1] <- "Plus"
+              end_type[k,2] <- "Minus"
+            } else if(which.min(as.matrix(Node_to_Pole)) == 2 && which.max(as.matrix(Node_to_Pole)) == 1){
+              end_type[k,1] <- "Minus"
+              end_type[k,2] <- "Plus"
+            } else if(which.min(as.matrix(Node_to_Pole)) == 3 && which.max(as.matrix(Node_to_Pole)) == 4){
+              end_type[k,1] <- "Plus"
+              end_type[k,2] <- "Minus"
+            } else if(which.min(as.matrix(Node_to_Pole)) == 4 && which.max(as.matrix(Node_to_Pole)) == 3){
+              end_type[k,1] <- "Minus"
+              end_type[k,2] <- "Plus"
+            } else {
+              end_type[k,1:2] <- NA
+            }
+          }
+          defin_end <- cbind(defin_end,
+                             end_type)
+          
+          for (k in 1:nrow(DF)){
+            if(is.na(defin_end[k,5]) || is.na(defin_end[k,6])){
+              defin_end[k,1:6] <- NA
+              DF[k,1:2] <- NA
+            } else {
+              if(defin_end[k,2] == DF[k,1] && defin_end[k,5] =="Minus" || defin_end[k,3] == DF[k,1] && defin_end[k,6] =="Minus"){
+                defin_end[k,1:6] <- NA
+                DF[k,1:2] <- NA
+              }
+            }
+          }
+          defin_end <- na.omit(defin_end)
+          DF <- na.omit(DF)
+        }
+        
         if(nrow(DF) > 0){
           defin_end <- cbind(paste(colnames(Segments)[x], i, sep = "_"),
                              defin_end[1],
@@ -70,14 +125,14 @@ Minus_end_seed <- function(x){
           Minus_end <- rbind(Minus_end,
                              defin_end)
         }
-        
       }
     }
+    
     list <- unique(Minus_end$Interactor_ID)
     Temp <- data.frame()
     for(k in list){
       DF <- Minus_end %>% filter_at(vars("Interactor_ID"),
-                              any_vars(. == k))
+                                    any_vars(. == k))
       Temp[k,1:7] <- DF[which.min(DF$p_to_P_dist), 1:7]
     }
     Minus_end <- na.omit(Temp)
