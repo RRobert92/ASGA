8c4ab1a3b704a03583038ed2e1a3455329e99aab
RRobert92 <robert.kiewisz@gmail.com>
2020-05-20 23:24:16 +0200

    Create Export_Data.R

---
+++ ASGA/bin/Utility/Export_Data.R
@@ -0,0 +1,23 @@
+################################################################################
+# Module Export_Data
+#
+# (c) 2019 Kiewisz
+# This code is licensed under GPL V3.0 license (see LICENSE.txt for details)
+#
+# Author: Robert Kiewisz
+# Created: 2020-05-20
+################################################################################
+
+
+Export_Data <- function (input, output, session){
+# Set Data dir -----------------------------------------------------------------
+  setwd("Data/")
+
+# Zip all file and cleand dir --------------------------------------------------
+  Zip_Files <- list.files(path = getwd(), pattern = ".xlsx$")
+  zip::zipr(zipfile = "ASGA_Data.zip", files = Zip_Files)
+  
+  file.remove(Zip_Files)
+# Back to main dir -------------------------------------------------------------
+  setwd("../")
+}
