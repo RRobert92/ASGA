8bb2d27145770bf7c1a55122fdaea21ecfa4f0a5
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2020-05-25 21:12:38 +0200

    Update T_Relative_Position.R

--- ASGA/bin/Tools/Pre_Analysis/T_Relative_Position.R
+++ ASGA/bin/Tools/Pre_Analysis/T_Relative_Position.R
@@ -4,8 +4,8 @@
 # The core tool to sort MT points based on their (+) and (-) end
 #
 # y variablein in the function is a DF name with points assinged to the MT (e.g. Pole1_01_1)
-# Function to work with Pole_1
-# Relative position of point between kinetochore and pole ( 1 - 0 ) for Pole_1
+# 
+# Relative position of point between kinetochore and pole ( 1 - 0 ) for Pole_1 and Pole_2
 #
 # (c) 2019 Kiewisz
 # This code is licensed under GPL V3.0 license (see LICENSE.txt for details)
@@ -31,7 +31,7 @@ relativ_pos_1 <- function(x, y){
   cbind(y, relat_pos)
 }
 
-# Relative Position for Pole1 ---------------------------------------------------------------------------
+# Relative Position for Pole2 ---------------------------------------------------------------------------
 relativ_pos_2 <- function(x, y){
   relativ_pos_part1 <- lapply(y[3], 
                               function(z){y[3] - Pole2[1,2]})
