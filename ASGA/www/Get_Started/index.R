5c3966fb4818c59f6c69673280f1c3b47fcd3ded
RRobert92 <robert.kiewisz@gmail.com>
2020-06-09 15:13:25 +0200

    Added upload for analysed data

--- ASGA/www/Get_Started/index.R
+++ ASGA/www/Get_Started/index.R
@@ -20,58 +20,67 @@ GetStarted_UI <- function(id){
     position = "fixed-top",
     id = "innavbar-GS",
     selected = "UploadData",
-
+    
 # UI-GetStarted-Home  ----------------------------------------------------------
 
-    tabPanel("Home", "Home"),
+tabPanel("Home", "Home"),
 
 # UI-GetStarted-UploadData  ----------------------------------------------------
 
-    tabPanel("Upload Data", value = "UploadData",
-             tags$div(class = "splash-container-GS",
-                      tags$div(class = "splash-GS",
-                               tags$h1(class = "splash-head-GS",
-                                       "Upload your files"),
-                               tags$p(class = "splash-subhead-GS",
-                                      "Upload multiple .xlsx files. For mor information see Wiki."),
-                               tags$div(class ="splash-input-GS",
-                                 fileInput(ns("file"), label = "",
-                                                  multiple = TRUE,
-                                                  accept = c(".xlsx", 
-                                                             "All"))
-                               )
-                      )
-             )
-    ),
+tabPanel("Upload Data", value = "UploadData",
+         tags$div(class = "splash-container-GS",
+                  tags$div(class = "splash-GS",
+                           tags$h1(class = "splash-head-GS",
+                                   "Upload your files"),
+                           tags$p(class = "splash-subhead-GS",
+                                  "Upload multiple .xlsx files. For mor information see Wiki."),
+                           tags$div(class = "splash-input-GS-row",
+                                    tags$div(class ="splash-input-GS",
+                                             "Upload Amira file for analysis:",
+                                             fileInput(ns("file"), label = "",
+                                                       multiple = TRUE,
+                                                       accept = c(".xlsx", 
+                                                                  "All"))),
+                                    tags$div(class ="splash-input-GS",        
+                                             "Upload analyzed data:",
+                                             fileInput(ns("file1"), label = "",
+                                                       multiple = TRUE,
+                                                       accept = c(".xlsx", 
+                                                                  "All"))
+                                    )
+                           )
+                  )
+         )
+),
 
 # UI-GetStarted-Settings  -----------------------------------------------------
 
-    tabPanel("Settings", value = "Settings",
-             tags$div(class = "splash-container-GS",
-                      tags$div(class = "splash-GS",
-                               tags$h1(class = "splash-head-GS",
-                                       "Set-Up the analysis")),
-                      tags$div(class = "table-GS",
-                               fluidRow(Setting_Buttons_UI("Home"),
-                                        column(7, 
-                                               uiOutput(ns("Tool_Info")
-                                                        )
-                                               )
-                                        ),
-                               tags$div(class = "table-GS-Center",
-                                        fluidRow(
-                                 actionBttn(
-                                   inputId = "Submit",
-                                   label = "Start Analysis", 
-                                   style = "material-flat",
-                                   color = "primary"
-                                 ),
-                                 tags$div(class = "asga-button asga-button-primary",
-                                           uiOutput(ns("Download_Button")))
-                               )
-                               )
-                      )
-             )
-    )
+tabPanel("Settings", value = "Settings",
+         tags$div(class = "splash-container-GS",
+                  tags$div(class = "splash-GS",
+                           tags$h1(class = "splash-head-GS",
+                                   "Set-Up the analysis")),
+                  tags$div(class = "table-GS",
+                           fluidRow(Setting_Buttons_UI("Home"),
+                                    column(7, 
+                                           uiOutput(ns("Tool_Info")
+                                           )
+                                    )
+                           ),
+                           tags$div(class = "table-GS-Center",
+                                    fluidRow(
+                                      actionBttn(
+                                        inputId = "Submit",
+                                        label = "Start Analysis", 
+                                        style = "material-flat",
+                                        color = "primary"
+                                      ),
+                                      tags$div(class = "asga-button asga-button-primary",
+                                               uiOutput(ns("Download_Button")))
+                                    )
+                           )
+                  )
+         )
+)
   )
 }
