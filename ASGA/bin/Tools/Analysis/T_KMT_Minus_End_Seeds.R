6de91c9e1cefab1187ed61449afa720f5c4ade68
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2020-07-03 14:55:47 +0200

    Update T_KMT_Minus_End_Seeds.R

--- ASGA/bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
+++ ASGA/bin/Tools/Analysis/T_KMT_Minus_End_Seeds.R
@@ -4,7 +4,7 @@
 # Function to calculate distribution of a (-) end in close proximity to the KMT
 #
 # The tool is calculating distance of each point on KMT (p) to every (-) end of SMT and KMT (P_S or P_K)
-# Then we associate point_id to segment_id of P_S or P_K which are closer to the p then 100nm 
+# Then we associate point_id to segment_id of P_S or P_K which are closer to the p then 55nm 
 # Multiple same segment_ID are marred to single entrance with their closes distance to the p
 #
 # (c) 2019 Kiewisz
