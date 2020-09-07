43c0e88af2a6dfffee4bcbafbfbe01444b876d89
RRobert92 <robert.kiewisz@gmail.com>
2020-09-07 09:17:18 +0200

    update loading of required packages

--- ASGA/global.r
+++ ASGA/global.r
@@ -70,6 +70,7 @@ source("bin/Packages/Analysis/A_KMT_Minus_End_Seeds.R")
 source("bin/Packages/Analysis/A_Fiber_Length_&_curvature.R")
 source("bin/Packages/Analysis/A_Kinetochore_Area.R")
 source("bin/Packages/Analysis/A_KMT_Torque.R")
+source("bin/Packages/Analysis/A_MT_Bridging.R")
 
 # Test Unit ---------------------------------------------------------------------
 source("tests/Load_Std_Data.R")
