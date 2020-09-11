506c84b15c07938fef216a08b35f418ec67fb475
RRobert92 <robert.kiewisz@gmail.com>
2020-09-11 16:25:15 +0200

    Bild a beginning of the package

--- ASGA/bin/Packages/Analysis/A_MT_Bridging.R
+++ ASGA/bin/Packages/Analysis/A_MT_Bridging.R
@@ -10,3 +10,47 @@
 ################################################################################
 
 # Set-up analysis --------------------------------------------------------------
+
+A_MT_Bridging <- function(input, output, session) {
+  progressSweetAlert(
+    session = session, id = "P_MT_Bridginig",
+    title = "Calculating microtubule interaction...",
+    display_pct = TRUE, value = round(0 / 3 * 100,0)
+  )
+  
+  # Calculate all interaction --------------------------------------------------
+  cores <- detectCores()
+  cl <- makeCluster(cores)
+  registerDoParallel(cl)
+  
+  MT_Interaction <<- foreach(i = 1:nrow(Points), .combine = rbind, .export = ls(.GlobalEnv)) %dopar% {
+  
+         Point_interaction(i)
+  }
+  stopCluster(cl)
+  
+  updateProgressBar(
+    session = session,
+    id = "P_MT_Bridginig",
+    title = "Pre-sorting data of MT interaction...",
+    value = round(1 / 3 * 100,0))
+  Sys.sleep(0.1)
+  
+  assign("MT_Interaction",
+         Segment_to_point(1),
+         envir = .GlobalEnv
+  )
+  
+  updateProgressBar(
+    session = session,
+    id = "P_MT_Bridginig",
+    value = round(2 / 3 * 100,0))
+  Sys.sleep(0.1)
+  
+  assign("MT_Interaction",
+         Segment_to_point(2),
+         envir = .GlobalEnv
+  )
+  
+  closeSweetAlert(session = session)
+}
--- ASGA/bin/Tools/Analysis/T_Bridging_MT.R
+++ ASGA/bin/Tools/Analysis/T_Bridging_MT.R
@@ -15,135 +15,114 @@
 ########################################################################################################
 
 # Function:  -------------------------------------------------------------------------------------------
-Point_interaction <- function() {
+Point_interaction <- function(x) {
 
   # Calculate distant of the point to other point in the boundary box ~15 min --------------------------
-  cores <- detectCores()
-  cl <- makeCluster(cores)
-  registerDoParallel(cl)
 
-  DF <- foreach(i = 1:nrow(Points), .combine = rbind) %dopar% {
-    p_to_P <- Points[with(Points, `X Coord` <= as.numeric(Points[i, 2] + (Minus_Distance)) &
-      `X Coord` >= as.numeric(Points[i, 2] - (Minus_Distance))), ]
-    p_to_P <- p_to_P[with(p_to_P, `Y Coord` <= as.numeric(Points[i, 3] + (Minus_Distance * 2)) &
-      `Y Coord` >= as.numeric(Points[i, 3] - (Minus_Distance))), ]
-    p_to_P <- p_to_P[with(p_to_P, `Z Coord` <= as.numeric(Points[i, 4] + (Minus_Distance * 2)) &
-      `Z Coord` >= as.numeric(Points[i, 4] - (Minus_Distance))), ]
-    p_to_P[5:7] <- Points[i, 2:4]
+  p_to_P <- Points[with(Points, `X Coord` <= as.numeric(Points[x, 2] + (Minus_Distance)) &
+    `X Coord` >= as.numeric(Points[x, 2] - (Minus_Distance))), ]
+  p_to_P <- p_to_P[with(p_to_P, `Y Coord` <= as.numeric(Points[x, 3] + (Minus_Distance * 2)) &
+    `Y Coord` >= as.numeric(Points[x, 3] - (Minus_Distance))), ]
+  p_to_P <- p_to_P[with(p_to_P, `Z Coord` <= as.numeric(Points[x, 4] + (Minus_Distance * 2)) &
+    `Z Coord` >= as.numeric(Points[x, 4] - (Minus_Distance))), ]
+  p_to_P[5:7] <- Points[
+    x,
+    2:4
+  ]
 
-    p_to_P$dist <- apply(
-      p_to_P[2:7],
-      1,
-      function(y) {
-        dist(matrix(y,
-          nrow = 2,
-          byrow = TRUE
-        ))
-      }
-    )
+  p_to_P$dist <- apply(
+    p_to_P[2:7],
+    1,
+    function(y) {
+      dist(matrix(y,
+        nrow = 2,
+        byrow = TRUE
+      ))
+    }
+  )
 
-    DF_1 <- data.frame(
-      p_to_P[with(p_to_P, dist <= Minus_Distance & dist > 0), "Point_ID"],
-      p_to_P[with(p_to_P, dist <= Minus_Distance & dist > 0), "dist"]
-    )
-    names(DF_1)[1] <- "Point_ID_2"
-    DF_1 <- data.frame(
-      Points[i, 1],
-      DF_1
-    )
-    names(DF_1)[1] <- "Point_ID_1"
-    names(DF_1)[3] <- "dist"
-    DF_1 <- cbind(DF_1, c(DF_1[1] - DF_1[2]))
-    names(DF_1)[4] <- "V1"
+  DF_1 <- data.frame(
+    p_to_P[
+      with(p_to_P, dist <= Minus_Distance & dist > 0),
+      "Point_ID"
+    ],
+    p_to_P[
+      with(p_to_P, dist <= Minus_Distance & dist > 0),
+      "dist"
+    ]
+  )
+  names(DF_1)[1] <- "Point_ID_2"
+  DF_1 <- data.frame(
+    Points[x, 1],
+    DF_1
+  )
+  names(DF_1)[1] <- "Point_ID_1"
+  names(DF_1)[3] <- "dist"
+  DF_1 <- cbind(DF_1, c(DF_1[1] - DF_1[2]))
+  names(DF_1)[4] <- "V1"
 
-    DF_1[with(DF_1, `V1` < -1 | `V1` > 1), 1:3]
-  }
-  stopCluster(cl)
+  DF_1[with(DF_1, `V1` < -1 | `V1` > 1), 1:3]
 }
 
-Segment_to_point <- function() {
+Segment_to_point <- function(x) {
   # Assign segments to the points ----------------------------------------------------------------------
-    t <- tibble(round(Segments$length/190, 0))
-    names(t)[1] <- "n"
-    t_df <- tibble()
-    for(i in 1:nrow(t)){
-      t_df[i,1] <- sum(t$n[1:i]) - 100
-    }
-    
+  Length_estiamtion <- tibble(round(Segments$length / 190, 0))
+  names(Length_estiamtion)[1] <- "no"
+  Length_estiamtion_df <- tibble()
+  for (i in 1:nrow(Length_estiamtion)) {
+    Length_estiamtion_df[i, 1] <- sum(Length_estiamtion$no[1:i]) - 100
+  }
+
   cores <- detectCores()
   cl <- makeCluster(cores)
   registerDoParallel(cl)
 
-  Segment_ID <- foreach(i = 1:nrow(DF), .combine = rbind) %dopar% {
-    
-     j = which.min(as.matrix(abs(t_df[1] - DF[i,1]))) - 5
-    if(j < 1){
-      j=1
+  Segment_ID <- foreach(
+    i = 1:nrow(MT_Interaction),
+    .combine = rbind,
+    .export = ls(.GlobalEnv)
+  ) %dopar% {
+    j <- which.min(as.matrix(abs(Length_estiamtion_df[1] - MT_Interaction[i, x]))) - 5
+    if (j < 1) {
+      j <- 1
     }
-   
 
     test_condition <- TRUE
     while (test_condition == TRUE) {
-      
-      if (0 < gregexpr(paste(",", as.character(DF[i, 1]), ",", sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1] || 
-          0 < gregexpr(paste(as.character(DF[i, 1]), ",", sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1] || 
-          0 < gregexpr(paste(",", as.character(DF[i, 1]), sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1]) {
-        
+      if (0 < gregexpr(paste(",", as.character(MT_Interaction[i, x]), ",", sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1] ||
+        0 < gregexpr(paste(as.character(MT_Interaction[i, x]), ",", sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1] ||
+        0 < gregexpr(paste(",", as.character(MT_Interaction[i, x]), sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1]) {
         test_condition <- FALSE
       } else {
-        
         j <- j + 1
       }
     }
-    
+
     Segment_ID_DF <- j - 1
     Segment_ID_DF
   }
   stopCluster(cl)
 
-  Segment_ID <- tibble(Segments_ID_1 = Segment_ID)
-  DF <- cbind(DF, Segment_ID)
+  Segment_ID <- tibble(Segments_ID = Segment_ID)
+  DF <- cbind(
+    MT_Interaction,
+    Segment_ID
+  )
 
-  
-  cores <- detectCores()
-  cl <- makeCluster(cores)
-  registerDoParallel(cl)
-  
-  Segment_ID <- foreach(i = 1:nrow(DF), .combine = rbind) %dopar% {
-    
-    j = which.min(as.matrix(abs(t_df[1] - DF[i,2]))) - 5
-    if(j < 1){
-      j=1
-    }
-    
-    
-    test_condition <- TRUE
-    while (test_condition == TRUE) {
-      
-      if (0 < gregexpr(paste(",", as.character(DF[i, 2]), ",", sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1] || 
-          0 < gregexpr(paste(as.character(DF[i, 2]), ",", sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1] || 
-          0 < gregexpr(paste(",", as.character(DF[i, 2]), sep = ""), Segments[j, "Point IDs"], fixed = T)[[1]][1]) {
-        
-        test_condition <- FALSE
-      } else {
-        
-        j <- j + 1
+  if (x == 2) {
+    Compare_ID <- tibble(Test = apply(
+      MT_Interaction[4:5],
+      1,
+      function(y) {
+        y[1] == y[2]
       }
-    }
-    
-    Segment_ID_DF <- j - 1
-    Segment_ID_DF
-  }
-  stopCluster(cl)
-  
-  Segment_ID <- tibble(Segments_ID_2 = Segment_ID)
-  DF <- cbind(DF, Segment_ID)
+    ))
 
-  Compare_ID <- tibble(Test = apply(DF[4:5], 1, function(y) {
-    y[1] == y[2]
-  }))
-  
-  DF <- cbind(DF, Compare_ID)
-  DF <- DF[with(DF, `Test` == FALSE),1:5]
-  rm(Compare_ID, Segment_ID,t, t_df)
+    DF <- cbind(
+      DF,
+      Compare_ID
+    )
+    DF <- DF[with(DF, `Test` == FALSE), 1:5]
+  }
+  DF
 }
