f5987bd82f5e9d70ee4a70d609f23d8a61c1b439
RRobert92 <robert.kiewisz@gmail.com>
2020-10-10 17:49:14 +0200

    update

--- ASGA/tests/Test_Output.R
+++ ASGA/tests/Test_Output.R
@@ -21,114 +21,123 @@ Run_Test <- function(input, output, session) {
     tryCatch(
       {
         callModule(Pre_Analysis, "Home")
-        Test_value[1, 1] <- TRUE
+        Test_value[1, 1] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 1] <- FALSE
+        Test_value[1, 1] <<- FALSE
       }
     )
-    names(Test_value)[1] <- "Pre_Analsis"
+    names(Test_value)[1] <<- "Pre_Analsis"
 
     tryCatch(
       {
         callModule(A_KMT_number, "Home")
-        Test_value[1, 2] <- TRUE
+        Test_value[1, 2] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 2] <- FALSE
+        Test_value[1, 2] <<- FALSE
       }
     )
-    names(Test_value)[2] <- "KMT_number"
+    names(Test_value)[2] <<- "KMT_number"
 
     tryCatch(
       {
         callModule(A_IKD, "Home")
-        Test_value[1, 3] <- TRUE
+        Test_value[1, 3] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 3] <- FALSE
+        Test_value[1, 3] <<- FALSE
       }
     )
-    names(Test_value)[3] <- "IKD"
+    names(Test_value)[3] <<- "IKD"
 
     tryCatch(
       {
         callModule(A_Curvature, "Home")
-        Test_value[1, 4] <- TRUE
+        Test_value[1, 4] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 4] <- FALSE
+        Test_value[1, 4] <<- FALSE
       }
     )
-    names(Test_value)[4] <- "Curvature"
+    names(Test_value)[4] <<- "Curvature"
 
     tryCatch(
       {
         callModule(A_End_Morphology, "Home")
-        Test_value[1, 5] <- TRUE
+        Test_value[1, 5] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 5] <- FALSE
+        Test_value[1, 5] <<- FALSE
       }
     )
-    names(Test_value)[5] <- "End_Morphology"
+    names(Test_value)[5] <<- "End_Morphology"
 
     tryCatch(
       {
         callModule(A_KMT_Torque, "Home")
-        Test_value[1, 6] <- TRUE
+        Test_value[1, 6] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 6] <- FALSE
+        Test_value[1, 6] <<- FALSE
       }
     )
-    names(Test_value)[6] <- "KMT_Torque"
+    names(Test_value)[6] <<- "KMT_Torque"
 
     tryCatch(
       {
         callModule(A_Fiber_Area, "Home")
-        Test_value[1, 7] <- TRUE
+        Test_value[1, 7] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 7] <- FALSE
+        Test_value[1, 7] <<- FALSE
       }
     )
-    names(Test_value)[7] <- "Fiber_area"
+    names(Test_value)[7] <<- "Fiber_area"
 
     tryCatch(
       {
         callModule(A_Fiber_Length_Curv, "Home")
-        Test_value[1, 8] <- TRUE
+        Test_value[1, 8] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 8] <- FALSE
+        Test_value[1, 8] <<- FALSE
       }
     )
-    names(Test_value)[8] <- "Fiber_L&C"
+    names(Test_value)[8] <<- "Fiber_L&C"
 
     tryCatch(
       {
         callModule(A_KMT_Minus_End_Seeds, "Home")
-        Test_value[1, 9] <- TRUE
+        Test_value[1, 9] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 9] <- FALSE
+        Test_value[1, 9] <<- FALSE
       }
     )
-    names(Test_value)[9] <- "Nucleation"
+    names(Test_value)[9] <<- "Nucleation"
 
     tryCatch(
       {
         callModule(A_K_Core_Area, "Home")
-        Test_value[1, 10] <- TRUE
+        Test_value[1, 10] <<- TRUE
       },
       error = function(e) {
-        Test_value[1, 10] <- FALSE
+        Test_value[1, 10] <<- FALSE
       }
     )
-    names(Test_value)[10] <- "K_Core_Area"
-
-    Test_value <<- Test_value
+    names(Test_value)[10] <<- "K_Core_Area"
+    
+    tryCatch(
+      {
+        callModule(Save_Data, "Home")
+        Test_value[1, 11] <<- TRUE
+      },
+      error = function(e) {
+        Test_value[1, 11] <<- FALSE
+      }
+    )
+    names(Test_value)[11] <<- "Save_data"
     File_name <<- as_tibble(ls(pattern = "Data_", envir = .GlobalEnv))
     File_name <<- na.omit(File_name)
   })
@@ -138,7 +147,7 @@ Run_Test <- function(input, output, session) {
 Test_Test <- function(input, output, session) {
   Test_df <<- data.frame()
   
-  Test_df[1, 1] <<- nrow(File_name) == 46
+  Test_df[1, 1] <<- nrow(File_name) == 43
   Test_df[1, 2] <<- ncol(Test_value) == length(Test_value[Test_value == TRUE])
 
   if (ncol(Test_df) == length((Test_df[Test_df == TRUE]))) {
