cf5a23ffbfad901185978ee0ce4664b2d00a5cd6
RRobert92 <robert.kiewisz@gmail.com>
2021-03-01 09:27:13 +0100

    fixed bug crashing calculation due to missing value in a loop

--- ASGA/bin/Packages/Analysis/A_MT_Bridging.R
+++ ASGA/bin/Packages/Analysis/A_MT_Bridging.R
@@ -19,45 +19,51 @@ A_MT_Bridging <- function(input, output, session) {
     display_pct = TRUE,
     value = round(0 / 4 * 100, 0)
   )
-  
-  #Define if analysis should be run as a function of distance or to run only for specified distance length
-  if(MT_INT_AS_FUNCTION == TRUE){
+
+  # Define if analysis should be run as a function of distance or to run only for specified distance length
+  if (MT_INT_AS_FUNCTION == TRUE) {
     Function_scale <<- list(0.025, 0.030, 0.035, 0.045, 0.050, 0.075, 0.1)
   } else {
     Function_scale <<- list(MT_POINT_CONFIG)
   }
-  
+
   # Calculate all interaction --------------------------------------------------
-  for (i in 1:length(Function_scale)) {
+  for (j in 1:length(Function_scale)) {
     updateProgressBar(
       session = session,
       id = "P_MT_Bridginig",
-      title = paste("Calculating microtubule interaction for", Function_scale[i], "um...", sep = " "),
-      value = round(as.numeric(i/length(Function_scale)) * 100, 0)
+      title = paste("Calculating microtubule interaction for", Function_scale[j], "um...", sep = " "),
+      value = round(as.numeric(j / length(Function_scale)) * 100, 0)
     )
     Sys.sleep(0.1)
     
+    MT_POINT_CONFIG <<- as.numeric(Function_scale[j])
 
-        cores <<- detectCores()
-        cl <<- makeCluster(cores)
-        registerDoParallel(cl)
-        
-        MT_Interaction <<- foreach(i = 1:nrow(Points), .combine = rbind, .inorder = FALSE, 
-                             .export = c("Points", "MT_POINT_CONFIG", "Point_interaction")) %dopar% {
-                Point_interaction(i)
-                }
-      stopCluster(cl)
+    cores <<- detectCores()
+    cl <<- makeCluster(cores)
+    registerDoParallel(cl)
+
+    MT_Interaction <<- foreach(
+      i = 1:nrow(Points), .combine = rbind, .inorder = FALSE,
+      .export = c("Points", "MT_POINT_CONFIG", "Point_interaction")
+    ) %dopar% {
+      Point_interaction(i)
+    }
+
+
+    stopCluster(cl)
     
     assign(paste("MT_Interaction", MT_POINT_CONFIG, sep = "_"),
-           MT_Interaction,
-           envir = .GlobalEnv)
+      MT_Interaction,
+      envir = .GlobalEnv
+    )
   }
 
   for (i in 1:length(Function_scale)) {
     MT_POINT_CONFIG <<- as.numeric(Function_scale[i])
-    Counter <<- list(str_split((as.numeric(1:4) + (4*(i-1))), pattern = " "))
+    Counter <<- list(str_split((as.numeric(1:4) + (4 * (i - 1))), pattern = " "))
     MT_Interaction <<- get(paste("MT_Interaction", MT_POINT_CONFIG, sep = "_"))
-      
+
     updateProgressBar(
       session = session,
       id = "P_MT_Bridginig",
@@ -65,10 +71,10 @@ A_MT_Bridging <- function(input, output, session) {
       value = round(((as.numeric(Counter[[1]][[1]])) / as.numeric(length(Function_scale) * 4)) * 100, 0)
     )
     Sys.sleep(0.1)
-    
+
     assign("MT_Interaction",
-           Segment_to_point(1),
-           envir = .GlobalEnv
+      Segment_to_point(1),
+      envir = .GlobalEnv
     )
 
     updateProgressBar(
@@ -77,14 +83,14 @@ A_MT_Bridging <- function(input, output, session) {
       value = round(((as.numeric(Counter[[1]][[2]]) / as.numeric(length(Function_scale) * 4))) * 100, 0)
     )
     Sys.sleep(0.1)
-    
+
     assign("MT_Interaction",
-           Segment_to_point(2),
-           envir = .GlobalEnv
+      Segment_to_point(2),
+      envir = .GlobalEnv
     )
-    
+
     names(MT_Interaction)[4:5] <<- c("Segments_ID_1", "Segments_ID_2")
-    
+
     updateProgressBar(
       session = session,
       title = paste("Remove duplicated interactions for", Function_scale[i], "um...", sep = " "),
@@ -92,12 +98,12 @@ A_MT_Bridging <- function(input, output, session) {
       value = round(((as.numeric(Counter[[1]][[3]]) / as.numeric(length(Function_scale) * 4))) * 100, 0)
     )
     Sys.sleep(0.1)
-    
+
     assign("MT_Interaction",
-           Remove_interaction_duplicates(),
-           envir = .GlobalEnv
+      Remove_interaction_duplicates(),
+      envir = .GlobalEnv
     )
-    
+
     updateProgressBar(
       session = session,
       title = paste("Searching for unique interacting points for", Function_scale[i], "um...", sep = " "),
@@ -105,12 +111,12 @@ A_MT_Bridging <- function(input, output, session) {
       value = round(((as.numeric(Counter[[1]][[4]]) / as.numeric(length(Function_scale) * 4))) * 100, 0)
     )
     Sys.sleep(0.1)
-    
+
     assign("MT_Interaction",
-           Unique_interaction(),
-           envir = .GlobalEnv
+      Unique_interaction(),
+      envir = .GlobalEnv
     )
-    
+
     updateProgressBar(
       session = session,
       title = paste("Saving final output for", Function_scale[i], "um...", sep = " "),
@@ -118,10 +124,11 @@ A_MT_Bridging <- function(input, output, session) {
       value = round(((as.numeric(Counter[[1]][[4]]) / as.numeric(length(Function_scale) * 4))) * 100, 0)
     )
     Sys.sleep(0.1)
-    
+
     assign(paste("MT_Interaction", MT_POINT_CONFIG, sep = "_"),
-           MT_Interaction,
-           envir = .GlobalEnv)
+      MT_Interaction,
+      envir = .GlobalEnv
+    )
   }
 
   closeSweetAlert(session = session)
